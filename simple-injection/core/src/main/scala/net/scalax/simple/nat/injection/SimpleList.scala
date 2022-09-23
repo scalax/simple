0ceef6af3af1d590459b0471eae6efdba27a305f
djx314 <djx314@sina.cn>
2022-09-24 05:16:51 +0800

    Implement list.

--- simple-injection/core/src/main/scala/net/scalax/simple/nat/injection/SimpleList.scala
+++ simple-injection/core/src/main/scala/net/scalax/simple/nat/injection/SimpleList.scala
@@ -10,13 +10,12 @@ abstract class SimpleZero[+T] extends SimpleList[T]
 object SimpleZero extends SimpleZero[Nothing] with impl.SimpleZeroImplObject {
   def unapply(u: SimpleList[Nothing]): Boolean = u match {
     case _: SimpleZero[_] => true
-    case _                => false
+    case _: SimpleAdd[_]  => false
   }
 }
 
 abstract class SimpleAdd[+T](val data: T, val tail: SimpleList[T]) extends SimpleList[T]
 object SimpleAdd {
-  import impl._
   def unapply[T](u: SimpleList[T]): Option[(T, SimpleList[T])] = u match {
     case s: SimpleAdd[T]  => Option((s.data, s.tail))
     case _: SimpleZero[_] => Option.empty
