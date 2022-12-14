37153bb9a01a94fbba4680b84a52fb60a1ed24b2
djx314 <djx314@sina.cn>
2022-12-15 03:53:46 +0800

    Fleur flutter Fleur flutter.

--- modules/main/simple-wire/jvm/src/main/scala-3/net/scalax/simple/nat/wire/MainApp.scala
+++ modules/main/simple-wire/jvm/src/main/scala-3/net/scalax/simple/nat/wire/MainApp.scala
@@ -7,21 +7,18 @@ import org.http4s.HttpApp
 import org.http4s.server.Server
 import org.http4s.implicits._
 import org.http4s.ember.server._
+import com.comcast.ip4s._
 import fs2.io.net.Network
-import constructor.HttpAppToLocalServer
 import constructor.AppWire
 
 object MainApp extends IOApp:
 
-  private val httpAppIO                            = for app <- AppWire.build yield app.orNotFound
-  private val serverIO                             = for given HttpApp[IO] <- httpAppIO yield new HttpAppToLocalServer[IO].server.build
-  private val serverResource: Resource[IO, Server] = for i1 <- Resource.eval(serverIO); i2 <- i1 yield i2
+  private val serverResource: Resource[IO, Server] = for
+    app    <- AppWire.build
+    server <- EmberServerBuilder.default[IO].withHttpApp(app.orNotFound).withHost(ipv4"0.0.0.0").withPort(port"8080").build
+  yield server
+  end serverResource
 
-  override def run(args: List[String]): IO[ExitCode] = serverResource
-    .use(_ => IO.never)
-    .recover { case e: Throwable =>
-      e.printStackTrace()
-    }
-    .as(ExitCode.Success)
+  override def run(args: List[String]): IO[ExitCode] = serverResource.use(_ => IO.never).as(ExitCode.Success)
 
 end MainApp
