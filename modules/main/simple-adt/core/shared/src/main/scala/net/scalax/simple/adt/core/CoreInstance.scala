a6f83a6eef6729e06e807c64e87a8d41833e48aa
djx314 <djx314@sina.cn>
2023-01-05 13:34:38 +0800

    pythagoras pythagoras

--- modules/main/simple-adt/core/shared/src/main/scala/net/scalax/simple/adt/core/CoreInstance.scala
+++ modules/main/simple-adt/core/shared/src/main/scala/net/scalax/simple/adt/core/CoreInstance.scala
@@ -3,6 +3,7 @@ package adt
 
 import core.Core2
 
+// Core2 实现自己有空改改，主要是 Function1 表达能力有限，最好这里的 def Core2 其实只是 Core2 的一个实例就可以表达
 object CoreInstance {
   private def Core2(func: (() => Core2) => Core2): Core2 = new Core2 {
     override def apply(v1: () => Core2): Core2 = func(v1)
@@ -22,7 +23,7 @@ object CoreInstance {
     override def apply(adtList: () => Core2): Core2 = adtList.apply().apply(() => tail())
   }
   object FoldListPositive {
-    def apply(func: (() => Core2) => FoldListPositive): Core2 = Core2(func)
+    def apply(func: (() => Core2) => Core2): Core2 = Core2(func)
   }
 
   private val FoldListZeroImpl: Core2 = Core2(tail => Core2(foldList => tail.apply().apply(() => foldList())))
