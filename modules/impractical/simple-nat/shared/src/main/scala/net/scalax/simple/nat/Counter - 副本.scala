21ce4a39ffd20fd74eb1f500d3be153ab296fa74
djx314 <djx314@sina.cn>
2023-01-14 16:09:35 +0800

    laboratory
    laboratory

---
+++ modules/impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/Counter - 副本.scala
@@ -0,0 +1,21 @@
+package net.scalax.simple
+package nat
+
+import core.Core2
+
+object NumberImpl1 {
+
+  def Core2(t: (() => Core2) => Core2): Core2 = new Core2 {
+    override def apply(v1: () => Core2): Core2 = t(v1)
+  }
+
+  val S: Core2 = Core2(tail => Core2(number => PosotiveCount(() => tail()(() => number()))))
+  val T: Core2 = Core2(tail => Core2(number => tail()(() => number())))
+  val U: Core2 = Core2(tail => Core2(number => PosotiveCount(() => number()(() => tail()))))
+  val V: Core2 = Core2(tail => Core2(number => number()(() => tail())))
+
+  case class PosotiveCount(tail: () => Core2) extends Core2 {
+    override def apply(t: () => Core2): Core2 = t()
+  }
+
+}
