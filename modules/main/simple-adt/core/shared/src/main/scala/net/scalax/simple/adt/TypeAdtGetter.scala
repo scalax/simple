0ddaebc207d795b6365bcde79573ecb7ecb142a4
djx314 <djx314@sina.cn>
2023-03-02 05:41:00 +0800

    Fix in `Number9`.

--- modules/main/simple-adt/core/shared/src/main/scala/net/scalax/simple/adt/TypeAdtGetter.scala
+++ modules/main/simple-adt/core/shared/src/main/scala/net/scalax/simple/adt/TypeAdtGetter.scala
@@ -1,9 +1,5 @@
 package net.scalax.simple.adt
 
 trait TypeAdtGetter {
-  def runGetter(adtConvert: TypeAdt.Context[Any, Any, Any]): Any
-}
-
-trait AdtValueGetter {
-  def value: Any
+  def executeFunction: Any => Any
 }
