dcbd75fa783276b802d6d1b9f3525402d68e661a
djx314 <djx314@sina.cn>
2023-03-13 18:43:03 +0800

    New Codec sample.

--- modules/main/simple-adt/core/shared/src/main/scala-2/net/scalax/simple/adt/AdtAlias.scala
+++ modules/main/simple-adt/core/shared/src/main/scala-2/net/scalax/simple/adt/AdtAlias.scala
@@ -3,6 +3,6 @@ package adt
 
 object AdtAlias extends impl.AdtAliasAbs {
   override type AdtZero                      = NumberCountZero
-  override type AdtNat                       = NumberCount[_]
+  override type AdtNat                       = NumberCount
   override type AdtAppend[A, Tail <: AdtNat] = NumberCountChild[A, Tail]
 }
--- modules/main/simple-adt/core/shared/src/main/scala-2/net/scalax/simple/adt/NumberCount.scala
+++ modules/main/simple-adt/core/shared/src/main/scala-2/net/scalax/simple/adt/NumberCount.scala
@@ -1,8 +1,6 @@
 package net.scalax.simple
 package adt
 
-import net.scalax.simple.core.Core1
-
-trait NumberCount[T <: NumberCount[_]]                     extends Core1[T]
-final class NumberCountChild[Head, Tail <: NumberCount[_]] extends NumberCount[Tail]
-final class NumberCountZero                                extends NumberCount[NumberCountZero]
+trait NumberCount
+final class NumberCountChild[Head, Tail <: NumberCount] extends NumberCount
+final class NumberCountZero                             extends NumberCount
