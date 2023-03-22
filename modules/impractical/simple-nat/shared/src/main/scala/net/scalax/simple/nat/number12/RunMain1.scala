ec4c70a249ea9a68c2aa4538f2b0056314338a83
djx314 <djx314@sina.cn>
2023-03-22 09:45:43 +0800

    Number.

--- modules/impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number12/RunMain1.scala
+++ modules/impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number12/RunMain1.scala
@@ -28,98 +28,31 @@ object RunMain1 extends App {
       numPositive2(other)
     }
 
-    val result1: BigDecimal = {
-      tag1 = 0
-      tag2 = 0
-      try {
-        numPositive1(() => numPositive2)
-      } catch {
-        case e: StackOverflowError =>
-      }
-      BigDecimal(tag2) / BigDecimal(tag1)
-    }
-
-    val result2: BigDecimal = {
-      tag1 = 0
-      tag2 = 0
-      try {
-        numPositive2(() => numPositive1)
-      } catch {
-        case e: StackOverflowError =>
-      }
-      BigDecimal(tag2) / BigDecimal(tag1)
-    }
-
-    val result3: BigDecimal = {
-      tag1 = 0
-      tag2 = 0
-      try {
-        numPositive1(() => numZero2)
-      } catch {
-        case e: StackOverflowError =>
-      }
-      BigDecimal(tag2) / BigDecimal(tag1)
-    }
-
-    val result4: BigDecimal = {
-      tag1 = 0
-      tag2 = 0
-      try {
-        numZero2(() => numPositive1)
-      } catch {
-        case e: StackOverflowError =>
-      }
-      BigDecimal(tag2) / BigDecimal(tag1)
-    }
-
-    val result5: BigDecimal = {
-      tag1 = 0
-      tag2 = 0
-      try {
-        numZero1(() => numPositive2)
-      } catch {
-        case e: StackOverflowError =>
-      }
-      BigDecimal(tag2) / BigDecimal(tag1)
-    }
-
-    val result6: BigDecimal = {
-      tag1 = 0
-      tag2 = 0
-      try {
-        numPositive2(() => numZero1)
-      } catch {
-        case e: StackOverflowError =>
-      }
-      BigDecimal(tag2) / BigDecimal(tag1)
-    }
-
-    val result7: BigDecimal = {
-      tag1 = 0
-      tag2 = 0
-      try {
-        numZero1(() => numZero2)
-      } catch {
-        case e: StackOverflowError =>
-      }
-      BigDecimal(tag2) / BigDecimal(tag1)
-    }
-
-    val result8: BigDecimal = {
-      tag1 = 0
-      tag2 = 0
-      try {
-        numZero2(() => numZero1)
-      } catch {
-        case e: StackOverflowError =>
-      }
-      BigDecimal(tag2) / BigDecimal(tag1)
-    }
-
     val result0: BigDecimal = BigDecimal(i1) / BigDecimal(i2)
+    val cha: BigDecimal     = BigDecimal("0.008")
+
+    def count(c: () => Core2): BigDecimal = {
+      tag1 = 0
+      tag2 = 0
+      try
+        c()
+      catch {
+        case e: StackOverflowError =>
+      }
+      BigDecimal(tag2) / BigDecimal(tag1)
+    }
+
+    val result1: BigDecimal = count(() => numPositive1(() => numPositive2))
+    val result2: BigDecimal = count(() => numPositive2(() => numPositive1))
+    val result3: BigDecimal = count(() => numPositive1(() => numZero2))
+    val result4: BigDecimal = count(() => numZero2(() => numPositive1))
+    val result5: BigDecimal = count(() => numZero1(() => numPositive2))
+    val result6: BigDecimal = count(() => numPositive2(() => numZero1))
+    val result7: BigDecimal = count(() => numZero1(() => numZero2))
+    val result8: BigDecimal = count(() => numZero2(() => numZero1))
+
     println(i1, i2, result0, result1, result2, result3, result4, result5, result6, result7, result8)
 
-    val cha = BigDecimal("0.008")
     assert((result0 - result1).abs < cha)
     assert((result0 - result2).abs < cha)
     assert((result0 - result3).abs < cha)
