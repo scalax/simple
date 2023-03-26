5cd0d970b1bd9450dab0590849307b990603a68a
djx314 <djx314@sina.cn>
2023-03-26 17:34:55 +0800

    一些Codec的例子。

--- wip/simple-codec/shared/src/test/scala/net/scala/simple/codec/XXaa.scala
+++ wip/simple-codec/shared/src/test/scala/net/scala/simple/codec/XXaa.scala
@@ -8,15 +8,17 @@ object xxbb {
   type UModel[T[_]] = Model[T]
 
   def main(arr: Array[String]): Unit = {
-    implicit val noneFiller: NoneModelFiller[UModel] = NoneModelFiller[UModel].generic
-    implicit val length: ModelLength[UModel]         = ModelLength[UModel].generic
-    implicit val setter: Setter[UModel]              = Setter[UModel].generic
-    val value: ContextO[UModel]#IdF = setter.input[ContextI#IdF](List(2, Option("nnuu"), classOf[String], "sdfjowiejrowehreiowjhrtf"))
+    implicit val noneFiller: NoneModelFiller[UModel]       = NoneModelFiller[UModel].generic
+    implicit val length: ModelLength[UModel]               = ModelLength[UModel].generic
+    implicit val setter: Setter[UModel]                    = Setter[UModel].generic
     implicit val namesImplicit: LabelledNames[UModel]      = LabelledNames[UModel].generic
     implicit val namedModel: LabelledInstance[UModel]      = LabelledInstance[UModel].generic
     implicit val modelListGetter: Getter[UModel]           = Getter[UModel].generic
     implicit val modelGetToMap: GetToMap[UModel]           = GetToMap[UModel].generic
     implicit val modelSetterFromMap: SetterFromMap[UModel] = SetterFromMap[UModel].generic
+
+    val value: ContextO[UModel]#IdF = setter.input[ContextI#IdF](List(2, Option("nnuu"), classOf[String], "sdfjowiejrowehreiowjhrtf"))
+
     println(length.length)
     println(length.length)
     println(namesImplicit.names)
