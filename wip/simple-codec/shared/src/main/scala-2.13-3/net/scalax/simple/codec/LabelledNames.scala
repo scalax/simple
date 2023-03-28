e0a3f051fcdb367eb95ef88bc467d007885f96e0
djx314 <djx314@sina.cn>
2023-03-28 08:20:26 +0800

    typo.

--- wip/simple-codec/shared/src/main/scala-2.13-3/net/scalax/simple/codec/LabelledNames.scala
+++ wip/simple-codec/shared/src/main/scala-2.13-3/net/scalax/simple/codec/LabelledNames.scala
@@ -3,7 +3,7 @@ package codec
 package impl
 
 trait LabelledNamesImplExtra {
-  class LabelledNamesImpl[F[_[_]]] {
+  trait LabelledNamesImplImpl[F[_[_]]] {
     def generic(implicit u: EmptyTagModelFiller[F], cv: F[ContextI#EmptyTagF] <:< Product): LabelledNames[F] = new LabelledNames[F] {
       override val names: List[String] = u.instance.productElementNames.to(List)
     }
