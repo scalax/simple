d6cb13bf0d9a4ebdbd24ea53f81f1614ccd1316c
djx314 <djx314@sina.cn>
2023-04-01 04:54:57 +0800

    typo fix.

---
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number14/Number.scala
@@ -0,0 +1,21 @@
+package net.scalax.simple.nat.number14
+
+trait Number1 {
+  def method(other: Number1 => Number1): Number2
+}
+case class Number1S(tail: () => Number1) extends Number1 {
+  override def method(other: Number1 => Number1): Number2 = other.method(tail())
+}
+case class Number1T(tail: () => Number1) extends Number1 {
+  override def method(other: Number1 => Number1): Number2 = Number2A(() => tail().method(other))
+}
+case class Number1U(tail: () => Number1) extends Number1 {
+  override def method(other: Number1 => Number1): Number2 = Number2B(() => tail().method(other))
+}
+case class Number1V(tail: () => Number1) extends Number1 {
+  override def method(other: Number1 => Number1): Number2 = Number2B(() => other.method(tail()))
+}
+
+sealed trait Number2
+case class Number2A(tail: () => Number2) extends Number2
+case class Number2B(tail: () => Number2) extends Number2
---
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number14/RunMain.scala
@@ -0,0 +1,42 @@
+package net.scalax.simple.nat.number14
+
+import scala.annotation.tailrec
+
+object RunMain extends App {
+
+  def count(numberA: Number1, numberB: Number1, except: BigDecimal): BigDecimal = {
+    var tag1: Long = 0
+    var tag2: Long = 0
+    @tailrec
+    def countImpl(c: () => Number2, initCount: Long): BigDecimal = {
+      val next: () => Number2 = c() match {
+        case Number2A(tail) =>
+          tag1 = tag1 + 1
+          tail
+        case Number2B(tail) =>
+          tag2 = tag2 + 1
+          tail
+      }
+      if (tag1 + tag2 < initCount)
+        countImpl(next, initCount)
+      else {
+        val resultBigDecimal: BigDecimal = BigDecimal(tag1) / BigDecimal(tag2)
+        val canOut: Boolean              = (resultBigDecimal - except).abs < BigDecimal("0.00001")
+        if (canOut)
+          resultBigDecimal
+        else
+          countImpl(next, initCount + 100000)
+      }
+    }
+    countImpl(() => numberA.method(numberB), 10000)
+  }
+
+  def numZeroRound(zero: () => Number1): Number1 = Number1T(() => Number1T(() => Number1T(() => Number1T(zero))))
+  def num1Round(num1: () => Number1): Number1 = {
+    class Number1X(val tail: () => Number1) extends Number1 {
+      override def method(other: Number1 => Number1): Number2 = Cut
+    }
+
+  }
+
+}
