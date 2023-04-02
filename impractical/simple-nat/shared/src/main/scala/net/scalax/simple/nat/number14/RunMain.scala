118244bc04812c199214a90d0fe8bbf629526413
djx314 <djx314@sina.cn>
2023-04-03 00:52:09 +0800

    temp2

--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number14/RunMain.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number14/RunMain.scala
@@ -3,7 +3,7 @@ package net.scalax.simple.nat.number14
 object RunMain extends App {
 
   try {
-    Number.n1Pos(() => Number.n2Pos)(() => Number.n4Pos)(() => Number.n3Pos)
+    Number.n1Pos(() => Number.n2Pos)(() => Number.n4Pos)(() => Number.n2Pos)
   } catch {
     case _: StackOverflowError =>
   }
