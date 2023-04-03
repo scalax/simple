2d8ec678afd6d71f9b95aa11aa7fbc26a9c30bd0
djx314 <djx314@sina.cn>
2023-04-03 15:17:26 +0800

    temp4

--- impractical/simple-nat/jvm/jvm.sbt
+++ impractical/simple-nat/jvm/jvm.sbt
@@ -4,7 +4,7 @@ name         := "simple-nat"
 enableZIOTest := true
 
 run / fork := true
-run / javaOptions += "-Xss155M"
+run / javaOptions += "-Xss600M"
 
 crossScalaVersions := Seq(scalaV.v211, scalaV.v212, scalaV.v213, scalaV.v3)
 
--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number14/RunMain.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number14/RunMain.scala
@@ -3,11 +3,12 @@ package net.scalax.simple.nat.number14
 object RunMain extends App {
 
   try {
-    Number.n1Pos(() => Number.n2Pos)(() => Number.n4Pos)(() => Number.n2Pos)
+    Number.n1Pos(() => Number.n2Pos)(() => Number.n4Pos)(() => Number.n3Pos)
   } catch {
     case _: StackOverflowError =>
   }
 
-  println(BigDecimal(Number.tag1) / BigDecimal(Number.tag2))
+  println(Number.tag1, Number.tag2)
+  println(BigDecimal(Number.tag1) / BigDecimal(Number.tag2 - 1))
 
 }
