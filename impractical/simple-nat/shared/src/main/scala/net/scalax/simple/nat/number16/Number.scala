b28da32fa681ef10387e82274b76e394307d6503
漓江丽火 <909134790@qq.com>
2023-04-16 08:41:43 +0800

    update pythagoras pythagoras.

---
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number16/Number.scala
@@ -0,0 +1,20 @@
+package net.scalax.simple.nat.number16
+
+import net.scalax.simple.ghdmzsk.ghdmzsk
+
+object Number1 {
+
+  var count1: Long = 0
+  var count2: Long = 0
+
+  val AA = ghdmzsk(tail =>
+    ghdmzsk { anything =>
+      count1 = count1 + 1
+      tail()
+    }
+  )
+
+  val number1S: ghdmzsk = ghdmzsk(tail => ghdmzsk(other => other()(tail)))
+  val number1T: ghdmzsk = ghdmzsk(tail => ghdmzsk(other => AA(() => other()(tail))))
+
+}
