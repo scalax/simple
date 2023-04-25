ab5c70382ab30a9920f7ac8044a6613b3438f7e5
漓江丽火 <909134790@qq.com>
2023-04-26 01:48:48 +0800

    pythagoras
    pythagoras python
    pythagoras.

--- wip/simple-codec/shared/src/main/scala-2.13-3/net/scalax/simple/codec/DefaultModelImplement.scala
+++ wip/simple-codec/shared/src/main/scala-2.13-3/net/scalax/simple/codec/DefaultModelImplement.scala
@@ -2,6 +2,5 @@ package net.scalax.simple
 package codec
 
 object DefaultModelImplement {
-  implicit def aaII[PolyInstance, T](implicit v: => T): ModelImplement[PolyInstance, T] =
-    new ModelImplement[PolyInstance, T](v)
+  implicit def aaII[PolyInstance, T](implicit v: => T): ModelImplement[PolyInstance, T] = new ModelImplement[PolyInstance, T](v)
 }
