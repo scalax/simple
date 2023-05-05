4ab352bea0088f31224c647afe413f75fcd04965
漓江丽火 <909134790@qq.com>
2023-05-06 03:52:42 +0800

    4564

---
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/group02/number_05/Number.scala
@@ -0,0 +1,43 @@
+package net.scalax.simple.nat.group02.number_05
+
+import net.scalax.simple.ghdmzsk.ghdmzsk
+
+object Number {
+
+  val number1S: ghdmzsk =
+    ghdmzsk(head =>
+      ghdmzsk(num1 => ghdmzsk(num2 => ghdmzsk(num3 => ghdmzsk(num4 => ghdmzsk(tail => AA(() => head()(num1)(num2)(num3)(num4)(tail)))))))
+    )
+  val number1T: ghdmzsk =
+    ghdmzsk(head =>
+      ghdmzsk(num1 => ghdmzsk(num2 => ghdmzsk(num3 => ghdmzsk(num4 => ghdmzsk(tail => num1()(num2)(num3)(num4)(tail)(head))))))
+    )
+  val number3S: ghdmzsk =
+    ghdmzsk(head =>
+      ghdmzsk(num1 => ghdmzsk(num2 => ghdmzsk(num3 => ghdmzsk(num4 => ghdmzsk(tail => tail()(head)(num1)(num2)(num3)(num4))))))
+    )
+  val number3T: ghdmzsk =
+    ghdmzsk(head =>
+      ghdmzsk(num1 => ghdmzsk(num2 => ghdmzsk(num3 => ghdmzsk(num4 => ghdmzsk(tail => BB(() => num1()(num2)(num3)(num4)(tail)(head)))))))
+    )
+
+  case class AA(cc: () => ghdmzsk) extends ghdmzsk {
+    override def apply(t: () => ghdmzsk): ghdmzsk = throw new Exception
+  }
+  case class BB(cc: () => ghdmzsk) extends ghdmzsk {
+    override def apply(t: () => ghdmzsk): ghdmzsk = throw new Exception
+  }
+
+  def genNumberImpl(positive: ghdmzsk, zero: ghdmzsk, count: Int): (ghdmzsk, ghdmzsk) = {
+    def genNumberImpl2(countImpl: Int): ghdmzsk = if (countImpl > 0) {
+      val tailValue = genNumberImpl2(countImpl = countImpl - 1)
+      positive(() => tailValue)
+    } else zeroValue
+
+    lazy val positiveValue: ghdmzsk = genNumberImpl2(count)
+    lazy val zeroValue: ghdmzsk     = zero(() => positiveValue)
+
+    (positiveValue, zeroValue)
+  }
+
+}
