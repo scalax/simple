12e54f59cc15e9b2241b1ffc853e70f4acf57ebf
漓江丽火 <909134790@qq.com>
2023-05-08 17:05:08 +0800

    4564

--- main/modules/simple-adt/implemention/shared/src/main/scala/net/scalax/simple/adt/implemention/Test.scala
+++ main/modules/simple-adt/implemention/shared/src/main/scala/net/scalax/simple/adt/implemention/Test.scala
@@ -1,61 +1,59 @@
 package net.scalax.simple.adt.implemention
 
-import net.scalax.simple.adt.factory.AdtCoreFactory
-import net.scalax.simple.ghdmzsk.ghdmzsk
+trait CoProductContext[Result] {
 
-sealed trait CoProductFunc[Result]
+  sealed trait Func
 
-object CoProductFunc {
-  class BuildContext[U] {
-    def appendSuccess[Data, T <: CoProductFunc[U]](data: Data): CoProductFuncPositive[Data, T, U] = new RightFunc[Data, T, U](data)
-    def appendFailed[Data, T <: CoProductFunc[U]](tail: T): CoProductFuncPositive[Data, T, U]     = new LeftFunc[Data, T, U](tail)
-    def end: CoProductFunc[U]                                                                     = new EmptyEnd[U]
+  sealed trait Positive[Data, T <: Func] extends Func {
+    def foldImpl(d: Data => Result): T
   }
 
-  def context[T]: BuildContext[T] = new BuildContext[T]
+  sealed trait Zero extends Func {
+    def default(d: => Result): Result
+    def option: Option[Result]
+  }
+
+  class LeftFunc[Data, T <: Func](val tail: T) extends Positive[Data, T] {
+    override def foldImpl(d: Data => Result): T = tail
+  }
+
+  class RightFunc[Data, T <: Func](val data: Data) extends Positive[Data, T] {
+    override def foldImpl(d: Data => Result): T = new EndFunc[Data, T](d(data)).asInstanceOf[T]
+  }
+
+  class EndFunc[Data, T <: Func](val result: Result) extends Positive[Data, T] with Zero {
+    override def foldImpl(d: Data => Result): T = this.asInstanceOf[T]
+    override def default(d: => Result): Result  = result
+    override def option: Some[Result]           = Some(result)
+  }
+
+  class EmptyEnd extends Zero {
+    override def default(d: => Result): Result = d
+    override def option: None.type             = None
+  }
+
+  object builder {
+    def appendSuccess[Data, T <: Func](data: Data): Positive[Data, T] = new RightFunc[Data, T](data)
+    def appendFailed[Data, T <: Func](tail: T): Positive[Data, T]     = new LeftFunc[Data, T](tail)
+    def end: Func                                                     = new EmptyEnd
+  }
+
 }
 
-sealed trait CoProductFuncPositive[Data, T <: CoProductFunc[Result], Result] extends CoProductFunc[Result] {
-  def foldImpl(d: Data => Result): T
-}
-sealed trait CoProductFuncZero[Result] extends CoProductFunc[Result] {
-  def default(d: => Result): Result
-  def option: Option[Result]
-}
-
-class LeftFunc[Data, T <: CoProductFunc[Result], Result](val tail: T) extends CoProductFuncPositive[Data, T, Result] {
-  override def foldImpl(d: Data => Result): T = tail
-}
-class RightFunc[Data, T <: CoProductFunc[Result], Result](val data: Data) extends CoProductFuncPositive[Data, T, Result] {
-  override def foldImpl(d: Data => Result): T = new EndFunc[Data, T, Result](d(data)).asInstanceOf[T]
-}
-class EndFunc[Data, T <: CoProductFunc[Result], Result](val result: Result)
-    extends CoProductFuncPositive[Data, T, Result]
-    with CoProductFuncZero[Result] {
-  override def foldImpl(d: Data => Result): T = this.asInstanceOf[T]
-  override def default(d: => Result): Result  = result
-  override def option: Some[Result]           = Some(result)
-}
-class EmptyEnd[Result] extends CoProductFuncZero[Result] {
-  override def default(d: => Result): Result = d
-  override def option: None.type             = None
+object CoProductContext {
+  private object CoProductContext extends CoProductContext[Any]
+  def build[T]: CoProductContext[T] = CoProductContext.asInstanceOf[CoProductContext[T]]
 }
 
 object Test extends App {
 
-  type Ux1 = CoProductFuncPositive[
-    Int,
-    CoProductFuncPositive[
-      List[String],
-      CoProductFuncPositive[List[Long], CoProductFuncPositive[List[String], CoProductFuncZero[List[Int]], List[Int]], List[Int]],
-      List[Int]
-    ],
-    List[Int]
-  ]
+  val c = CoProductContext.build[List[Int]]
 
-  val bContext = CoProductFunc.context[List[Int]]
+  type Ux1 = c.Positive[Int, c.Positive[List[String], c.Positive[List[Long], c.Positive[List[String], c.Zero]]]]
 
-  val p: Ux1 = bContext.appendFailed(bContext.appendFailed(bContext.appendSuccess(List(2L, 3L, 5L, 8L))))
+  val builder = c.builder
+
+  val p: Ux1 = builder.appendFailed(builder.appendFailed(builder.appendSuccess(List(2L, 3L, 5L, 8L))))
 
   val t =
     p.foldImpl(t => List(t)).foldImpl(t => t.map(_.size + 1)).foldImpl(t => t.map(_.toInt * 2)).foldImpl(t => t.map(_.size * 5)).option
