c226efe432d8a1415ea798a2e6c940dcc9889814
漓江丽火 <909134790@qq.com>
2023-05-19 04:44:17 +0800

    Update sbt-depts version.

--- main/modules/simple-adt/implemention/shared/src/main/scala/net/scalax/simple/adt/implemention/Test.scala
+++ main/modules/simple-adt/implemention/shared/src/main/scala/net/scalax/simple/adt/implemention/Test.scala
@@ -2,88 +2,99 @@ package net.scalax.simple.adt.implemention
 
 import scala.collection.compat._
 
-trait CoProductContext[Result] {
-
-  trait NatFunc
-  trait NatFuncPositive[Data, T <: NatFunc] extends NatFunc {
-    def foldImpl(d: Data => Result): T
-  }
-  trait NatFuncZero extends NatFunc {
-    def default(d: => Result): Result
-    def option: Option[Result]
-  }
-
-  trait InputFunc[T <: NatFunc] {
-    def input(r: Result): T
-  }
-  case class InputPositive[Data, T <: NatFunc](tail: InputFunc[T]) extends InputFunc[NatFuncPositive[Data, T]] {
-    override def input(r: Result): NatFuncPositive[Data, T] = utils.EndFunc(r, tail)
-  }
-  case object InputZero extends InputFunc[NatFuncZero] {
-    override def input(r: Result): ZeroSuccessed = ZeroSuccessed(r)
-  }
-
-  case class LeftFunc[Data, T <: NatFunc](val tail: T) extends NatFuncPositive[Data, T] {
-    override def foldImpl(d: Data => Result): T = tail
-  }
-  case class RightFunc[Data, T <: NatFunc](data: Data, tail: InputFunc[T]) extends NatFuncPositive[Data, T] {
-    override def foldImpl(d: Data => Result): T = tail.input(d(data))
-  }
-  case object ZeroFailed extends NatFuncZero {
-    override def default(d: => Result): Result = d
-    override val option: None.type             = None
-  }
-  case class ZeroSuccessed(resultData: Result) extends NatFuncZero {
-    override def default(d: => Result): Result = resultData
-    override val option: Some[Result]          = Some(resultData)
-  }
-
-  private object utils {
-    case class EndFunc[Data, T <: NatFunc](result: Result, tail: InputFunc[T]) extends NatFuncPositive[Data, T] {
-      override def foldImpl(d: Data => Result): T = tail.input(result)
-    }
-  }
-
+trait FoldContext[Result] {
+  def default(d: => Result): Result
+  def option: Option[Result]
+  def rewire(d: => Result): FoldContext[Result]
+}
+object FoldContext {
+  def init[Result]: FoldContext[Result] = new EmptyFoldContext
 }
 
-object CoProductContext {
-  private class Co[T] extends CoProductContext[T]
-  def build[T]: CoProductContext[T] = new Co[T]
+class EmptyFoldContext[Result] extends FoldContext[Result] {
+  override def default(d: => Result): Result             = d
+  override def option: None.type                         = None
+  override def rewire(d: => Result): FoldContext[Result] = new FinishedFoldContext(d)
 }
+class FinishedFoldContext[Result](val value: Result) extends FoldContext[Result] {
+  override def default(d: => Result): Result             = value
+  override def option: Some[Result]                      = Some(value)
+  override def rewire(d: => Result): FoldContext[Result] = new FinishedFoldContext(d)
+}
+
+trait NatFunc
+trait NatFuncPositive[Data, T <: NatFunc] extends NatFunc {
+  def foldImpl[Result](d: Data => Result, fc: FoldContext[Result]): (T, FoldContext[Result])
+}
+trait NatFuncZero extends NatFunc
+
+case class LeftFunc[Data, T <: NatFunc](tail: T) extends NatFuncPositive[Data, T] {
+  override def foldImpl[Result](d: Data => Result, fc: FoldContext[Result]): (T, FoldContext[Result]) = (tail, fc)
+}
+case class RightFunc[Data, T <: NatFunc](data: Data, tail: T) extends NatFuncPositive[Data, T] {
+  override def foldImpl[Result](d: Data => Result, fc: FoldContext[Result]): (T, FoldContext[Result]) = (tail, fc.rewire(d(data)))
+}
+case object ZeroFailed extends NatFuncZero
 
 object Test extends App {
 
-  val c = CoProductContext.build[List[Int]]
+  type Ux1 = NatFuncPositive[
+    Int,
+    NatFuncPositive[List[String], NatFuncPositive[List[Long], NatFuncPositive[List[String], NatFuncPositive[String, NatFuncZero]]]]
+  ]
+  type Ux2 = NatFuncPositive[Int, NatFuncPositive[List[String], NatFuncPositive[List[Long], NatFuncPositive[List[String], NatFuncZero]]]]
+  type Ux3 = NatFuncPositive[Int, NatFuncPositive[List[String], NatFuncPositive[List[Long], NatFuncZero]]]
 
-  type Ux1 =
-    c.NatFuncPositive[Int, c.NatFuncPositive[
-      List[String],
-      c.NatFuncPositive[List[Long], c.NatFuncPositive[List[String], c.NatFuncPositive[String, c.NatFuncZero]]]
-    ]]
-  type Ux2 =
-    c.NatFuncPositive[Int, c.NatFuncPositive[List[String], c.NatFuncPositive[List[Long], c.NatFuncPositive[List[String], c.NatFuncZero]]]]
-  type Ux3 = c.NatFuncPositive[Int, c.NatFuncPositive[List[String], c.NatFuncPositive[List[Long], c.NatFuncZero]]]
+  val p1: Ux1 = LeftFunc(LeftFunc(RightFunc(List(2L, 3L, 5L, 8L), LeftFunc(LeftFunc(ZeroFailed)))))
+  val p2: Ux2 = LeftFunc(LeftFunc(RightFunc(List(2L, 3L, 5L, 8L), LeftFunc(ZeroFailed))))
+  val p3: Ux3 = LeftFunc(LeftFunc(RightFunc(List(2L, 3L, 5L, 8L), ZeroFailed)))
+  val p4: Ux2 = LeftFunc(LeftFunc(LeftFunc(LeftFunc(ZeroFailed))))
+  val p5: Ux2 = LeftFunc(RightFunc(List("abc", "abcdefghij", "abcdefghijklmn"), LeftFunc(LeftFunc(ZeroFailed))))
 
-  val p1: Ux1 = c.LeftFunc(c.LeftFunc(c.RightFunc(List(2L, 3L, 5L, 8L), c.InputPositive(c.InputPositive(c.InputZero)))))
-  val p2: Ux2 = c.LeftFunc(c.LeftFunc(c.RightFunc(List(2L, 3L, 5L, 8L), c.InputPositive(c.InputZero))))
-  val p3: Ux3 = c.LeftFunc(c.LeftFunc(c.RightFunc(List(2L, 3L, 5L, 8L), c.InputZero)))
-  val p4: Ux2 = c.LeftFunc(c.LeftFunc(c.LeftFunc(c.LeftFunc(c.ZeroFailed))))
-  val p5: Ux2 = c.LeftFunc(c.RightFunc(List("abc", "abcdefghij", "abcdefghijklmn"), c.InputPositive(c.InputPositive(c.InputZero))))
+  val t1 = {
+    val (p1_1, p1_2) = (p1, FoldContext.init[List[Int]])
+    val (p2_1, p2_2) = p1_1.foldImpl(t => List(t), p1_2)
+    val (p3_1, p3_2) = p2_1.foldImpl(t => t.map(_.size + 1), p2_2)
+    val (p4_1, p4_2) = p3_1.foldImpl(t => t.map(_.toInt * 2), p3_2)
+    val (p5_1, p5_2) = p4_1.foldImpl(t => t.map(_.size * 5), p4_2)
+    val (p6_1, p6_2) = p5_1.foldImpl(t => t.to(List).map(_.toInt * 2), p5_2)
+    p6_2.option
+  }
 
-  val t1 =
-    p1.foldImpl(t => List(t))
-      .foldImpl(t => t.map(_.size + 1))
-      .foldImpl(t => t.map(_.toInt * 2))
-      .foldImpl(t => t.map(_.size * 5))
-      .foldImpl(t => t.to(List).map(_.toInt * 2))
-      .option
-  val t2 =
-    p2.foldImpl(t => List(t)).foldImpl(t => t.map(_.size + 1)).foldImpl(t => t.map(_.toInt * 2)).foldImpl(t => t.map(_.size * 5)).option
-  val t3 = p3.foldImpl(t => List(t)).foldImpl(t => t.map(_.size + 1)).foldImpl(t => t.map(_.toInt * 2)).option
-  val t4 =
-    p4.foldImpl(t => List(t)).foldImpl(t => t.map(_.size + 1)).foldImpl(t => t.map(_.toInt * 2)).foldImpl(t => t.map(_.size * 5)).option
-  val t5 =
-    p5.foldImpl(t => List(t)).foldImpl(t => t.map(_.size + 1)).foldImpl(t => t.map(_.toInt * 2)).foldImpl(t => t.map(_.size * 5)).option
+  val t2 = {
+    val (p1_1, p1_2) = (p2, FoldContext.init[List[Int]])
+    val (p2_1, p2_2) = p1_1.foldImpl(t => List(t), p1_2)
+    val (p3_1, p3_2) = p2_1.foldImpl(t => t.map(_.size + 1), p2_2)
+    val (p4_1, p4_2) = p3_1.foldImpl(t => t.map(_.toInt * 2), p3_2)
+    val (p5_1, p5_2) = p4_1.foldImpl(t => t.map(_.size * 5), p4_2)
+    p5_2.option
+  }
+
+  val t3 = {
+    val (p1_1, p1_2) = (p3, FoldContext.init[List[Int]])
+    val (p2_1, p2_2) = p1_1.foldImpl(t => List(t), p1_2)
+    val (p3_1, p3_2) = p2_1.foldImpl(t => t.map(_.size + 1), p2_2)
+    val (p4_1, p4_2) = p3_1.foldImpl(t => t.map(_.toInt * 2), p3_2)
+    p4_2.option
+  }
+
+  val t4 = {
+    val (p1_1, p1_2) = (p4, FoldContext.init[List[Int]])
+    val (p2_1, p2_2) = p1_1.foldImpl(t => List(t), p1_2)
+    val (p3_1, p3_2) = p2_1.foldImpl(t => t.map(_.size + 1), p2_2)
+    val (p4_1, p4_2) = p3_1.foldImpl(t => t.map(_.toInt * 2), p3_2)
+    val (p5_1, p5_2) = p4_1.foldImpl(t => t.map(_.size * 5), p4_2)
+    p5_2.option
+  }
+
+  val t5 = {
+    val (p1_1, p1_2) = (p5, FoldContext.init[List[Int]])
+    val (p2_1, p2_2) = p1_1.foldImpl(t => List(t), p1_2)
+    val (p3_1, p3_2) = p2_1.foldImpl(t => t.map(_.size + 1), p2_2)
+    val (p4_1, p4_2) = p3_1.foldImpl(t => t.map(_.toInt * 2), p3_2)
+    val (p5_1, p5_2) = p4_1.foldImpl(t => t.map(_.size * 5), p4_2)
+    p5_2.option
+  }
 
   println(t1) // Some(List(4, 6, 10, 16))
   println(t2) // Some(List(4, 6, 10, 16))
