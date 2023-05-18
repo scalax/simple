4989d9c591b667e08b97f02c206ecb772a8dad18
漓江丽火 <909134790@qq.com>
2023-05-19 06:47:00 +0800

    Update sbt-depts version.

--- main/modules/simple-adt/implemention/shared/src/main/scala/net/scalax/simple/adt/implemention/Test.scala
+++ main/modules/simple-adt/implemention/shared/src/main/scala/net/scalax/simple/adt/implemention/Test.scala
@@ -5,36 +5,44 @@ import scala.collection.compat._
 trait FoldContext[Result] {
   def default(d: => Result): Result
   def option: Option[Result]
-  def rewire(d: => Result): FoldContext[Result]
+  def overrideOnce(d: => Result): FoldContext[Result]
 }
 object FoldContext {
   def init[Result]: FoldContext[Result] = new EmptyFoldContext
 }
 
 class EmptyFoldContext[Result] extends FoldContext[Result] {
-  override def default(d: => Result): Result             = d
-  override def option: None.type                         = None
-  override def rewire(d: => Result): FoldContext[Result] = new FinishedFoldContext(d)
+  override def default(d: => Result): Result                   = d
+  override def option: None.type                               = None
+  override def overrideOnce(d: => Result): FoldContext[Result] = new FinishedFoldContext(d)
 }
 class FinishedFoldContext[Result](val value: Result) extends FoldContext[Result] {
-  override def default(d: => Result): Result             = value
-  override def option: Some[Result]                      = Some(value)
-  override def rewire(d: => Result): FoldContext[Result] = new FinishedFoldContext(d)
+  override def default(d: => Result): Result                   = value
+  override def option: Some[Result]                            = Some(value)
+  override def overrideOnce(d: => Result): FoldContext[Result] = this
 }
 
 trait NatFunc
 trait NatFuncPositive[Data, T <: NatFunc] extends NatFunc {
   def foldImpl[Result](d: Data => Result, fc: FoldContext[Result]): (T, FoldContext[Result])
 }
-trait NatFuncZero extends NatFunc
+
+class IsFinishAndNothing {
+  lazy val isFinishAndNothing: IsFinishAndNothing = this
+}
+object IsFinishAndNothing extends IsFinishAndNothing
 
 case class LeftFunc[Data, T <: NatFunc](tail: T) extends NatFuncPositive[Data, T] {
   override def foldImpl[Result](d: Data => Result, fc: FoldContext[Result]): (T, FoldContext[Result]) = (tail, fc)
 }
 case class RightFunc[Data, T <: NatFunc](data: Data, tail: T) extends NatFuncPositive[Data, T] {
-  override def foldImpl[Result](d: Data => Result, fc: FoldContext[Result]): (T, FoldContext[Result]) = (tail, fc.rewire(d(data)))
+  override def foldImpl[Result](d: Data => Result, fc: FoldContext[Result]): (T, FoldContext[Result]) = (tail, fc.overrideOnce(d(data)))
 }
-case object ZeroFailed extends NatFuncZero
+class NatFuncZero extends NatFuncPositive[IsFinishAndNothing.type, NatFuncZero] {
+  def foldImpl[Result](d: IsFinishAndNothing.type => Result, fc: FoldContext[Result]): (NatFuncZero, FoldContext[Result]) =
+    (this, fc.overrideOnce(d(IsFinishAndNothing)))
+}
+object NatFuncZero extends NatFuncZero
 
 object Test extends App {
 
@@ -45,11 +53,11 @@ object Test extends App {
   type Ux2 = NatFuncPositive[Int, NatFuncPositive[List[String], NatFuncPositive[List[Long], NatFuncPositive[List[String], NatFuncZero]]]]
   type Ux3 = NatFuncPositive[Int, NatFuncPositive[List[String], NatFuncPositive[List[Long], NatFuncZero]]]
 
-  val p1: Ux1 = LeftFunc(LeftFunc(RightFunc(List(2L, 3L, 5L, 8L), LeftFunc(LeftFunc(ZeroFailed)))))
-  val p2: Ux2 = LeftFunc(LeftFunc(RightFunc(List(2L, 3L, 5L, 8L), LeftFunc(ZeroFailed))))
-  val p3: Ux3 = LeftFunc(LeftFunc(RightFunc(List(2L, 3L, 5L, 8L), ZeroFailed)))
-  val p4: Ux2 = LeftFunc(LeftFunc(LeftFunc(LeftFunc(ZeroFailed))))
-  val p5: Ux2 = LeftFunc(RightFunc(List("abc", "abcdefghij", "abcdefghijklmn"), LeftFunc(LeftFunc(ZeroFailed))))
+  val p1: Ux1 = LeftFunc(LeftFunc(RightFunc(List(2L, 3L, 5L, 8L), LeftFunc(LeftFunc(NatFuncZero)))))
+  val p2: Ux2 = LeftFunc(LeftFunc(RightFunc(List(2L, 3L, 5L, 8L), LeftFunc(NatFuncZero))))
+  val p3: Ux3 = LeftFunc(LeftFunc(RightFunc(List(2L, 3L, 5L, 8L), NatFuncZero)))
+  val p4: Ux2 = LeftFunc(LeftFunc(LeftFunc(LeftFunc(NatFuncZero))))
+  val p5: Ux2 = LeftFunc(RightFunc(List("abc", "abcdefghij", "abcdefghijklmn"), LeftFunc(LeftFunc(NatFuncZero))))
 
   val t1 = {
     val (p1_1, p1_2) = (p1, FoldContext.init[List[Int]])
@@ -58,7 +66,21 @@ object Test extends App {
     val (p4_1, p4_2) = p3_1.foldImpl(t => t.map(_.toInt * 2), p3_2)
     val (p5_1, p5_2) = p4_1.foldImpl(t => t.map(_.size * 5), p4_2)
     val (p6_1, p6_2) = p5_1.foldImpl(t => t.to(List).map(_.toInt * 2), p5_2)
-    p6_2.option
+    val (p7_1, p7_2) = p6_1.foldImpl(
+      { t =>
+        t.isFinishAndNothing
+        List.empty
+      },
+      p6_2
+    )
+    val (p8_1, p8_2) = p7_1.foldImpl(
+      { t =>
+        t.isFinishAndNothing
+        List.empty
+      },
+      p7_2
+    )
+    p8_2.option
   }
 
   val t2 = {
