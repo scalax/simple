66eead5f135abaff7b9d1fc224fd23535602d0a1
漓江丽火 <909134790@qq.com>
2023-06-04 00:21:20 +0800

    Fix code. WIP.

--- main/modules/simple-adt/implemention/shared/src/main/scala/net/scalax/simple/adt/implemention/Test.scala
+++ main/modules/simple-adt/implemention/shared/src/main/scala/net/scalax/simple/adt/implemention/Test.scala
@@ -2,24 +2,43 @@ package net.scalax.simple.adt.implemention
 
 import scala.collection.compat._
 
-trait FoldContext[+Result] {
-  def option: Option[Result]
-  def overrideOnce[D, U >: Result](d: DataInstance[D])(func: D => U): FoldContext[U]
-}
-object FoldContext {
-  val empty: FoldContext[Nothing] = EmptyFoldContext
+object UnapplyInstance {
 
-  private object EmptyFoldContext extends FoldContext[Nothing] {
-    override val option: None.type = None
-    override def overrideOnce[D, U](d: DataInstance[D])(func: D => U): FoldContext[U] = {
-      val v = for (dValue <- d.dataInstance) yield new FinishedFoldContext(func(dValue))
-      v.getOrElse(this)
+  trait CountNatPositive[T1 <: TypeCounter, -T2 <: NatFunc] {
+    def input(n: T2): Option[DataType]
+    type DataType
+  }
+  object CountNatPositive extends CountNatPositiveImplicitRaw1 {
+    type Aux[T1 <: TypeCounter, -T2 <: NatFunc, DType] = CountNatPositive[T1, T2] {
+      type DataType = DType
     }
+
+    implicit def zeroGetTail[T2Tail <: NatFunc, Data]: CountNatPositive.Aux[PositiveTypeCounterZero, NatFuncPositive[Data, T2Tail], Data] =
+      new CountNatPositive[PositiveTypeCounterZero, NatFuncPositive[Data, T2Tail]] {
+        override def input(n: NatFuncPositive[Data, T2Tail]): Option[Data] = n.dataInstance
+        override type DataType = Data
+      }
   }
-  private class FinishedFoldContext[+Result](val value: Result) extends FoldContext[Result] {
-    override def option: Some[Result]                                                           = Some(value)
-    override def overrideOnce[D, U >: Result](d: DataInstance[D])(func: D => U): FoldContext[U] = this
+  trait CountNatPositiveImplicitRaw1 {
+    implicit def positiveGetTail[T1Tail <: TypeCounter, T2Tail <: NatFunc, Data, TempData](implicit
+      t: CountNatPositive.Aux[T1Tail, T2Tail, Data]
+    ): CountNatPositive.Aux[PositiveTypeCounter[T1Tail], NatFuncPositive[TempData, T2Tail], Data] =
+      new CountNatPositive[PositiveTypeCounter[T1Tail], NatFuncPositive[TempData, T2Tail]] {
+        override def input(n: NatFuncPositive[TempData, T2Tail]): Option[Data] = t.input(n.tail)
+
+        override type DataType = Data
+      }
   }
+
+  sealed trait TypeCounter
+  class PositiveTypeCounter[T <: TypeCounter] extends TypeCounter
+  class PositiveTypeCounterZero               extends PositiveTypeCounter[PositiveTypeCounterZero]
+
+  class UnapplyFunc[T <: TypeCounter](t: T) {
+    def unapply[U <: NatFunc, Data](u: U)(implicit t: CountNatPositive.Aux[T, U, Data]): Option[Data] = t.input(u)
+  }
+  def UnapplyFunc[T <: TypeCounter](t: T): UnapplyFunc[T] = new UnapplyFunc(t)
+
 }
 
 trait DataInstance[Data] {
@@ -28,39 +47,29 @@ trait DataInstance[Data] {
 }
 
 trait NatFunc
-abstract case class NatFuncPositive[Data, +T <: NatFunc](override val dataInstance: Option[Data]) extends NatFunc with DataInstance[Data] {
+abstract case class NatFuncPositive[Data, T <: NatFunc](override val dataInstance: Option[Data]) extends NatFunc with DataInstance[Data] {
   override val isDefined: Boolean = dataInstance.isDefined
   def tail: T
 }
 
 object NatFunc {
   def success[D, T <: NatFunc](t: D, tail: T): NatFuncPositive[D, T] = new NatFuncPositiveSuccess(data = t, tail = tail)
-  def successValue[D, T <: NatFunc](t: D): NatFuncPositive[D, T]     = new NatFuncPositiveSuccessImpl(data = t)
   def empty[D, T <: NatFunc](tail: T): NatFuncPositive[D, T]         = new NatFuncPositiveEmpty(tail = tail)
-  private def emptyInstance[T <: NatFunc]: T                         = emptyNone.asInstanceOf[T]
   val zero: NatFuncZero                                              = NatFuncZero.value
 
-  private class NatFuncPositiveSuccess[Data, +T <: NatFunc](data: Data, override val tail: T)
-      extends NatFuncPositive[Data, T](dataInstance = Some(data)) {
-    override val isDefined: Boolean = true
-  }
-  private class NatFuncPositiveSuccessImpl[Data, +T <: NatFunc](data: Data)
-      extends NatFuncPositiveSuccess[Data, T](data = data, tail = emptyNone.asInstanceOf[T]) {
-    override val isDefined: Boolean = true
-  }
-  private class NatFuncPositiveEmpty[Data, +T <: NatFunc](override val tail: T)
-      extends NatFuncPositive[Data, T](dataInstance = Option.empty) {
-    override val isDefined: Boolean = false
-  }
+  private class NatFuncPositiveSuccess[Data, T <: NatFunc](data: Data, override val tail: T)
+      extends NatFuncPositive[Data, T](dataInstance = Some(data))
+  private class NatFuncPositiveEmpty[Data, T <: NatFunc](override val tail: T) extends NatFuncPositive[Data, T](dataInstance = Option.empty)
 
   private lazy val emptyNone: NatFuncPositive[Any, NatFunc] = new NatFuncPositive[Any, NatFunc](dataInstance = None) {
     override lazy val tail: NatFunc = emptyNone
-    override val isDefined: Boolean = false
   }
 }
 
 final class IsFinishAndNothing {
-  def matchErrorAndNothing: Nothing = throw new Exception("match error.")
+  def default[T](t: => T): T           = t
+  lazy val isEnded: IsFinishAndNothing = this
+  def matchErrorAndNothing: Nothing    = throw new Exception("match error.")
 }
 object IsFinishAndNothing {
   lazy val value: IsFinishAndNothing = new IsFinishAndNothing
@@ -89,48 +98,92 @@ object Test extends App {
   val p4: Ux2 = NatFunc.empty(NatFunc.empty(NatFunc.empty(NatFunc.empty(NatFunc.zero))))
   val p5: Ux2 = NatFunc.empty(NatFunc.success(List("abc", "abcdefghij", "abcdefghijklmn"), NatFunc.empty(NatFunc.empty(NatFunc.zero))))
 
-  val t1 = FoldContext.empty
-    .overrideOnce(p1)(t => List(t))
-    .overrideOnce(p1.tail)(t => t.map(_.size + 1))
-    .overrideOnce(p1.tail.tail)(t => t.map(_.toInt * 2))
-    .overrideOnce(p1.tail.tail.tail)(t => t.map(_.size * 5))
-    .overrideOnce(p1.tail.tail.tail.tail)(t => t.to(List).map(_.toInt * 2))
-    .overrideOnce(p1.tail.tail.tail.tail.tail) { t =>
-      def doneSomething: Nothing = t.matchErrorAndNothing
-      List.empty
-    }
-    .overrideOnce(p1.tail.tail.tail.tail.tail.tail) { t =>
-      def doneSomething: Nothing = t.matchErrorAndNothing
-      List.empty
-    }
-    .option
+  val Options1 = UnapplyInstance.UnapplyFunc(new UnapplyInstance.PositiveTypeCounterZero)
+  val Options2 = UnapplyInstance.UnapplyFunc(new UnapplyInstance.PositiveTypeCounter[UnapplyInstance.PositiveTypeCounterZero])
+  val Options3 = UnapplyInstance.UnapplyFunc(
+    new UnapplyInstance.PositiveTypeCounter[UnapplyInstance.PositiveTypeCounter[UnapplyInstance.PositiveTypeCounterZero]]
+  )
+  val Options4 = UnapplyInstance.UnapplyFunc(
+    new UnapplyInstance.PositiveTypeCounter[
+      UnapplyInstance.PositiveTypeCounter[UnapplyInstance.PositiveTypeCounter[UnapplyInstance.PositiveTypeCounterZero]]
+    ]
+  )
+  val Options5 = UnapplyInstance.UnapplyFunc(
+    new UnapplyInstance.PositiveTypeCounter[UnapplyInstance.PositiveTypeCounter[
+      UnapplyInstance.PositiveTypeCounter[UnapplyInstance.PositiveTypeCounter[UnapplyInstance.PositiveTypeCounterZero]]
+    ]]
+  )
+  val Options6 = UnapplyInstance.UnapplyFunc(
+    new UnapplyInstance.PositiveTypeCounter[UnapplyInstance.PositiveTypeCounter[UnapplyInstance.PositiveTypeCounter[
+      UnapplyInstance.PositiveTypeCounter[UnapplyInstance.PositiveTypeCounter[UnapplyInstance.PositiveTypeCounterZero]]
+    ]]]
+  )
+  val Options7 = UnapplyInstance.UnapplyFunc(
+    new UnapplyInstance.PositiveTypeCounter[
+      UnapplyInstance.PositiveTypeCounter[UnapplyInstance.PositiveTypeCounter[UnapplyInstance.PositiveTypeCounter[
+        UnapplyInstance.PositiveTypeCounter[UnapplyInstance.PositiveTypeCounter[UnapplyInstance.PositiveTypeCounterZero]]
+      ]]]
+    ]
+  )
 
-  val t2 = FoldContext.empty
-    .overrideOnce(p2)(t => List(t))
-    .overrideOnce(p2.tail)(t => t.map(_.size + 1))
-    .overrideOnce(p2.tail.tail)(t => t.map(_.toInt * 2))
-    .overrideOnce(p2.tail.tail.tail)(t => t.map(_.size * 5))
-    .option
+  val preT1 = p1 match {
+    case Options1(t) => List(t)
+    case Options2(t) => t.map(_.size + 1)
+    case Options3(t) => t.map(_.toInt * 2)
+    case Options4(t) => t.map(_.size * 5)
+    case Options5(t) => t.to(List).map(_.toInt * 2)
+    case Options6(t) => t.matchErrorAndNothing
+    case Options7(t) => t.matchErrorAndNothing
+  }
 
-  val t3 = FoldContext.empty
-    .overrideOnce(p3)(t => List(t))
-    .overrideOnce(p3.tail)(t => t.map(_.size + 1))
-    .overrideOnce(p3.tail.tail)(t => t.map(_.toInt * 2))
-    .option
+  val t1: List[Int] = preT1
 
-  val t4 = FoldContext.empty
-    .overrideOnce(p4)(t => List(t))
-    .overrideOnce(p4.tail)(t => t.map(_.size + 1))
-    .overrideOnce(p4.tail.tail)(t => t.map(_.toInt * 2))
-    .overrideOnce(p4.tail.tail.tail)(t => t.map(_.size * 5))
-    .option
+  val preT2 = p2 match {
+    case Options1(t) => List(t)
+    case Options2(t) => t.map(_.size + 1)
+    case Options3(t) => t.map(_.toInt * 2)
+    case Options4(t) => t.map(_.size * 5)
+    case Options5(t) => t.matchErrorAndNothing
+    case Options6(t) => t.matchErrorAndNothing
+  }
 
-  val t5 = FoldContext.empty
-    .overrideOnce(p5)(t => List(t))
-    .overrideOnce(p5.tail)(t => t.map(_.size + 1))
-    .overrideOnce(p5.tail.tail)(t => t.map(_.toInt * 2))
-    .overrideOnce(p5.tail.tail.tail)(t => t.map(_.size * 5))
-    .option
+  val t2: List[Int] = preT2
+
+  val preT3 = p3 match {
+    case Options1(t) => List(t)
+    case Options2(t) => t.map(_.size + 1)
+    case Options3(t) => t.map(_.toInt * 2)
+    case Options4(t) => t.matchErrorAndNothing
+    case Options5(t) => t.matchErrorAndNothing
+  }
+
+  val t3: List[Int] = preT3
+
+  val preT4 = p4 match {
+    case Options1(t) => Some(List(t))
+    case Options2(t) => Some(t.map(_.size + 1))
+    case Options3(t) => Some(t.map(_.toInt * 2))
+    case Options4(t) => Some(t.map(_.size * 5))
+    case Options5(t) =>
+      t.isEnded
+      Option.empty
+    case Options6(t) =>
+      t.isEnded
+      Option.empty
+  }
+
+  val t4: Option[List[Int]] = preT4
+
+  val preT5 = p5 match {
+    case Options1(t) => List(t)
+    case Options2(t) => t.map(_.size + 1)
+    case Options3(t) => t.map(_.toInt * 2)
+    case Options4(t) => t.map(_.size * 5)
+    case Options5(t) => t.matchErrorAndNothing
+    case Options6(t) => t.matchErrorAndNothing
+  }
+
+  val t5: List[Int] = preT5
 
   println(t1) // Some(List(4, 6, 10, 16))
   println(t2) // Some(List(4, 6, 10, 16))
