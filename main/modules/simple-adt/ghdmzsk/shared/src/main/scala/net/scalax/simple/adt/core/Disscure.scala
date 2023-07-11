3e829927fba6e693c9899d658156774389348911
漓江丽火 <909134790@qq.com>
2023-07-12 03:58:25 +0800

    ghdmzsk test.

---
+++ main/modules/simple-adt/ghdmzsk/shared/src/main/scala/net/scalax/simple/adt/core/Disscure.scala
@@ -0,0 +1,82 @@
+package net.scalax.simple.adt
+package test
+
+import scala.collection.compat._
+
+import net.scalax.simple.ghdmzsk.ghdmzsk
+
+object Disscure {
+
+  val emptyGhdmzsk: ghdmzsk = new ghdmzsk {
+    override def inputGHDMZSK(t: => ghdmzsk): ghdmzsk = throw new Exception("xuejd")
+  }
+
+  val identityGhdmzsk: ghdmzsk = new ghdmzsk {
+    override def inputGHDMZSK(t: => ghdmzsk): ghdmzsk = t
+  }
+
+  val takeProperty: ghdmzsk = new ghdmzsk {
+    override def inputGHDMZSK(tail: => ghdmzsk): ghdmzsk = new ghdmzsk {
+      override def inputGHDMZSK(func: => ghdmzsk): ghdmzsk = new ghdmzsk {
+        override def inputGHDMZSK(selector: => ghdmzsk): ghdmzsk = selector.inputGHDMZSK(tail).inputGHDMZSK(func)
+      }
+    }
+  }
+
+  val takeTail: ghdmzsk = new ghdmzsk {
+    override def inputGHDMZSK(tail: => ghdmzsk): ghdmzsk = new ghdmzsk {
+      override def inputGHDMZSK(func: => ghdmzsk): ghdmzsk = tail
+    }
+  }
+  val takeHead: ghdmzsk = new ghdmzsk {
+    override def inputGHDMZSK(tail: => ghdmzsk): ghdmzsk = new ghdmzsk {
+      override def inputGHDMZSK(func: => ghdmzsk): ghdmzsk = func
+    }
+  }
+
+  val a1Impl1: ghdmzsk = new ghdmzsk {
+    override def inputGHDMZSK(tail: => ghdmzsk): ghdmzsk = {
+      val func1: ghdmzsk = new ghdmzsk {
+        override def inputGHDMZSK(data: => ghdmzsk): ghdmzsk = tail.inputGHDMZSK(data)
+      }
+      val func2: ghdmzsk = new ghdmzsk {
+        override def inputGHDMZSK(other: => ghdmzsk): ghdmzsk = other.inputGHDMZSK(tail)
+      }
+      val num2: ghdmzsk = takeProperty.inputGHDMZSK(emptyGhdmzsk).inputGHDMZSK(func2)
+      val num1: ghdmzsk = takeProperty.inputGHDMZSK(num2).inputGHDMZSK(func1)
+      num1.inputGHDMZSK(takeHead)
+    }
+  }
+
+  val a1VImpl: ghdmzsk = new ghdmzsk {
+    override def inputGHDMZSK(tail: => ghdmzsk): ghdmzsk = {
+      val func2: ghdmzsk = new ghdmzsk {
+        override def inputGHDMZSK(other: => ghdmzsk): ghdmzsk = other.inputGHDMZSK(tail)
+      }
+      val num2: ghdmzsk = takeProperty.inputGHDMZSK(emptyGhdmzsk).inputGHDMZSK(func2)
+      val num1: ghdmzsk = takeProperty.inputGHDMZSK(num2).inputGHDMZSK(aZero)
+      num1.inputGHDMZSK(takeHead)
+    }
+  }
+
+  val aZero: ghdmzsk = new ghdmzsk with GetValue {
+    override def value: Any                           = "wowowower爱爱啊"
+    override def inputGHDMZSK(t: => ghdmzsk): ghdmzsk = t.inputGHDMZSK(aZero)
+  }
+
+  val numData: ghdmzsk =
+    a1Impl1.inputGHDMZSK(a1Impl1.inputGHDMZSK(a1Impl1.inputGHDMZSK(a1Impl1.inputGHDMZSK(a1Impl1.inputGHDMZSK(a1VImpl)))))
+
+  val func1ToUse = numData
+
+  trait GetValue {
+    def value: Any
+  }
+
+  def main(arr: Array[String]): Unit = {
+    val r        = func1ToUse.inputGHDMZSK(identityGhdmzsk)
+    val valueGet = r.asInstanceOf[GetValue].value
+    println(valueGet)
+  }
+
+}
