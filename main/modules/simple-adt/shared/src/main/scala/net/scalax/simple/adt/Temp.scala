ae831304c2da5b52858c43e94d4c17ee225f894e
漓江丽火 <909134790@qq.com>
2023-07-24 16:41:58 +0800

    x

--- main/modules/simple-adt/shared/src/main/scala/net/scalax/simple/adt/Temp.scala
+++ main/modules/simple-adt/shared/src/main/scala/net/scalax/simple/adt/Temp.scala
@@ -13,18 +13,28 @@ trait ToGHDMZSK {
   def toGHDMZSK: ghdmzsk
 }
 
-trait ADTData[+N <: AdtNat] extends ToGHDMZSK {
+sealed trait Status
+object Status {
+  class Passed extends Status
+  object Passed
+
+  class NotFinished extends Status
+  object NotFinished
+}
+
+trait ADTData[+N <: AdtNat, S <: Status] extends ToGHDMZSK {
   override def toGHDMZSK: ghdmzsk
 }
 
 object ADTData {
-  def success[D, T <: AdtNat](data: D, tail: ADTData[T]): ADTData[AdtNatData[D, T]] = new ADTData[AdtNatData[D, T]] {
-    override val toGHDMZSK: ghdmzsk = Disscure.a1VImpl(data).inputGHDMZSK(tail.toGHDMZSK)
-  }
-  def empty[D, T <: AdtNat](tail: ADTData[T]): ADTData[AdtNatData[D, T]] = new ADTData[AdtNatData[D, T]] {
+  def success[D, T <: AdtNat, S <: Status](data: D, tail: ADTData[T, S]): ADTData[AdtNatData[D, T], Status.Passed] =
+    new ADTData[AdtNatData[D, T], Status.Passed] {
+      override val toGHDMZSK: ghdmzsk = Disscure.a1VImpl(data).inputGHDMZSK(tail.toGHDMZSK)
+    }
+  def empty[D, T <: AdtNat, S <: Status](tail: ADTData[T, S]): ADTData[AdtNatData[D, T], S] = new ADTData[AdtNatData[D, T], S] {
     override val toGHDMZSK: ghdmzsk = Disscure.a1Impl1.inputGHDMZSK(tail.toGHDMZSK)
   }
-  lazy val zero: ADTData[AdtNatZero] = new ADTData[AdtNatZero] {
+  lazy val zero: ADTData[AdtNatZero, Status.NotFinished] = new ADTData[AdtNatZero, Status.NotFinished] {
     override lazy val toGHDMZSK: ghdmzsk = Disscure.a1VImpl(IsFinishAndNothing.value).inputGHDMZSK(zero.toGHDMZSK)
   }
 }
--- main/modules/simple-adt/shared/src/main/scala/net/scalax/simple/adt/TypeAdtApply.scala
+++ main/modules/simple-adt/shared/src/main/scala/net/scalax/simple/adt/TypeAdtApply.scala
@@ -1,9 +1,8 @@
 package net.scalax.simple
 package adt
 
-import net.scalax.simple.ghdmzsk.ghdmzsk
-import implemention._
 import temp._
+import temp.{Status => TStatus}
 
 /** TODO
   *
@@ -13,16 +12,12 @@ import temp._
   * @since 2022/08/28
   *   02:48
   */
-class TypeAdtApply[Input, Sum <: AdtNat](val value: Boolean => Input => ADTData[Sum]) {
-  type State <: TypeAdt.Status
-}
+class TypeAdtApply[Input, Sum <: AdtNat, S <: TStatus](val value: Boolean => Input => ADTData[Sum, S])
 
 object TypeAdtApply extends impl.TypeAdtImplicitOptsPolyHigher {
-  type Aux[Input, Sum <: AdtNat, S <: TypeAdt.Status] = TypeAdtApply[Input, Sum] { type State = S }
-  def apply[Input, Sum <: AdtNat, S <: TypeAdt.Status](value: Boolean => Input => ADTData[Sum]): Aux[Input, Sum, S] =
-    new TypeAdtApply[Input, Sum](value) {
-      override type State = S
-    }
+  def apply[Input, Sum <: AdtNat, S <: TypeAdt.Status, ST <: TStatus](
+    value: Boolean => Input => ADTData[Sum, ST]
+  ): TypeAdtApply[Input, Sum, ST] = new TypeAdtApply[Input, Sum, ST](value)
 }
 
 package impl {
@@ -34,38 +29,35 @@ package impl {
   }
 
   trait TypeAdtImplicitOptsPolyHigher extends HListTypeAdtPositiveLower1 {
-    @inline implicit def hlistTypeAdtPositiveImplicit1[A, B, Tail <: AdtNat, AdtConvertPoly, Status <: TypeAdt.Status](implicit
+    @inline implicit def hlistTypeAdtPositiveImplicit1[A, B, Tail <: AdtNat, AdtConvertPoly, ST <: TStatus](implicit
       adtConvert: TypeAdt.Context[A, B, AdtConvertPoly],
-      tailMapping: TypeAdtApply.Aux[A, Tail, Status]
-    ): TypeAdtApply.Aux[A, AdtNatData[TypeAdt.Adapter[B, AdtConvertPoly], Tail], TypeAdt.Status.Passed] = {
+      tailMapping: TypeAdtApply[A, Tail, ST]
+    ): TypeAdtApply[A, AdtNatData[TypeAdt.Adapter[B, AdtConvertPoly], Tail], TStatus.Passed] = {
       val adtConvertImpl = new AdapterContext(adtConvert)
       TypeAdtApply(isOk => i => ADTData.success(adtConvertImpl.input(i), tailMapping.value(true)(i)))
     }
   }
 
   trait HListTypeAdtPositiveLower1 extends HListTypeAdtPositiveLower2 {
-    @inline implicit def hlistTypeAdtPositiveImplicit2[A, B, Tail <: AdtNat, Status <: TypeAdt.Status](implicit
+    @inline implicit def hlistTypeAdtPositiveImplicit2[A, B, Tail <: AdtNat, ST <: TStatus](implicit
       adtConvert: TypeAdt.Context[A, B, DefaultAdtContext.type],
-      tailMapping: TypeAdtApply.Aux[A, Tail, Status]
-    ): TypeAdtApply.Aux[A, AdtNatData[B, Tail], TypeAdt.Status.Passed] =
+      tailMapping: TypeAdtApply[A, Tail, ST]
+    ): TypeAdtApply[A, AdtNatData[B, Tail], TStatus.Passed] =
       TypeAdtApply(isOk => i => ADTData.success(adtConvert.input(i), tailMapping.value(true)(i)))
   }
 
   trait HListTypeAdtPositiveLower2 extends LowerLevelPoly {
-    @inline implicit def hlistTypeMappingPositiveImplicitLower[A, B, Status <: TypeAdt.Status, Tail <: AdtNat](implicit
-      tailMapping: TypeAdtApply.Aux[A, Tail, Status]
-    ): TypeAdtApply.Aux[A, AdtNatData[B, Tail], Status] =
-      TypeAdtApply(isOk => i => ADTData.empty(tailMapping.value(isOk)(i)))
+    @inline implicit def hlistTypeMappingPositiveImplicitLower[A, B, Tail <: AdtNat, ST <: TStatus](implicit
+      tailMapping: TypeAdtApply[A, Tail, ST]
+    ): TypeAdtApply[A, AdtNatData[B, Tail], ST] = TypeAdtApply(isOk => i => ADTData.empty(tailMapping.value(isOk)(i)))
   }
 
   trait LowerLevelPoly {
-    private object failedValue extends TypeAdtApply[Any, AdtNatZero](value = isOk => i => ADTData.zero) {
-      override type State = Adt.Status.Failed
-    }
-    private def failedValueImpl[T]: TypeAdtApply.Aux[T, AdtNatZero, Adt.Status.Failed] =
-      failedValue.asInstanceOf[TypeAdtApply.Aux[T, AdtNatZero, Adt.Status.Failed]]
+    private object failedValue extends TypeAdtApply[Any, AdtNatZero, TStatus.NotFinished](value = isOk => i => ADTData.zero)
+    private def failedValueImpl[T]: TypeAdtApply[T, AdtNatZero, TStatus.NotFinished] =
+      failedValue.asInstanceOf[TypeAdtApply[T, AdtNatZero, TStatus.NotFinished]]
 
-    implicit def adtFailedResult[I]: TypeAdtApply.Aux[I, AdtNatZero, Adt.Status.Failed] = failedValueImpl
+    implicit def adtFailedResult[I]: TypeAdtApply[I, AdtNatZero, TStatus.NotFinished] = failedValueImpl
   }
 
 }
