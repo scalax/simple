5d269015ae31ffe595e216313d58cb4ad50f9f60
漓江丽火 <909134790@qq.com>
2023-07-30 01:56:54 +0800

    Code fix.

--- main/modules/simple-adt/shared/src/main/scala/net/scalax/simple/adt/TypeAdtApply.scala
+++ main/modules/simple-adt/shared/src/main/scala/net/scalax/simple/adt/TypeAdtApply.scala
@@ -12,19 +12,25 @@ import impl.Adt.{Status => ADTStatus}
   * @since 2022/08/28
   *   02:48
   */
-class TypeAdtApply[Input, Sum <: AdtNat, ST <: ADTStatus](val value: Input => ADTData[Sum, ST])
+class TypeAdtApply[Input, Sum <: AdtNat, ST <: ADTStatus](val value: Input => ADTData[Sum, ST]) extends AnyVal
 
-object TypeAdtApply extends impl.TypeAdtImplicitOptsPolyHigher {
-  def apply[Input, Sum <: AdtNat, ST <: ADTStatus](value: Input => ADTData[Sum, ST]): TypeAdtApply[Input, Sum, ST] =
-    new TypeAdtApply[Input, Sum, ST](value)
-}
+object TypeAdtApply extends impl.TypeAdtImplicitOptsPolyHigher
 
 package impl {
 
-  class AdapterContext[A, B, AdtConvertPoly](private val c: TypeAdt.Context[A, B, AdtConvertPoly])
-      extends AnyVal
-      with TypeAdt.Context[A, TypeAdt.Adapter[B, AdtConvertPoly], AdtConvertPoly] {
-    override def input(t: A): TypeAdt.Adapter[B, AdtConvertPoly] = new TypeAdt.Adapter[B, AdtConvertPoly](c.input(t))
+  private object Helper {
+    def adapterContext[A, B, AdtConvertPoly](
+      c: TypeAdt.Context[A, B, AdtConvertPoly]
+    ): TypeAdt.Context[A, TypeAdt.Adapter[B, AdtConvertPoly], AdtConvertPoly] =
+      new TypeAdt.Context[A, TypeAdt.Adapter[B, AdtConvertPoly], AdtConvertPoly] {
+        override def input(t: A): TypeAdt.Adapter[B, AdtConvertPoly] = new TypeAdt.Adapter[B, AdtConvertPoly](c.input(t))
+      }
+
+    private val failedValue: TypeAdtApply[Any, AdtNatZero, ADTStatus.NotFinished] =
+      new TypeAdtApply[Any, AdtNatZero, ADTStatus.NotFinished](value = i => ADTData.zero)
+
+    def failedValueImpl[T]: TypeAdtApply[T, AdtNatZero, ADTStatus.NotFinished] =
+      failedValue.asInstanceOf[TypeAdtApply[T, AdtNatZero, ADTStatus.NotFinished]]
   }
 
   trait TypeAdtImplicitOptsPolyHigher extends HListTypeAdtPositiveLower1 {
@@ -32,8 +38,8 @@ package impl {
       adtConvert: TypeAdt.Context[A, B, AdtConvertPoly],
       tailMapping: TypeAdtApply[A, Tail, ST]
     ): TypeAdtApply[A, AdtNatPositive[TypeAdt.Adapter[B, AdtConvertPoly], Tail], ADTStatus.Passed] = {
-      val adtConvertImpl = new AdapterContext(adtConvert)
-      TypeAdtApply(i => ADTData.success(adtConvertImpl.input(i), tailMapping.value(i)))
+      val adtConvertImpl = Helper.adapterContext(adtConvert)
+      new TypeAdtApply(i => ADTData.success(adtConvertImpl.input(i), tailMapping.value(i)))
     }
   }
 
@@ -41,22 +47,19 @@ package impl {
     @inline implicit def hlistTypeAdtPositiveImplicit2[A, B, Tail <: AdtNat, ST <: ADTStatus](implicit
       adtConvert: TypeAdt.Context[A, B, DefaultAdtContext.type],
       tailMapping: TypeAdtApply[A, Tail, ST]
-    ): TypeAdtApply[A, AdtNatPositive[B, Tail], ADTStatus.Passed] =
-      TypeAdtApply(i => ADTData.success(adtConvert.input(i), tailMapping.value(i)))
+    ): TypeAdtApply[A, AdtNatPositive[B, Tail], ADTStatus.Passed] = new TypeAdtApply(i =>
+      ADTData.success(adtConvert.input(i), tailMapping.value(i))
+    )
   }
 
   trait HListTypeAdtPositiveLower2 extends LowerLevelPoly {
     @inline implicit def hlistTypeMappingPositiveImplicitLower[A, B, Tail <: AdtNat, ST <: ADTStatus](implicit
       tailMapping: TypeAdtApply[A, Tail, ST]
-    ): TypeAdtApply[A, AdtNatPositive[B, Tail], ST] = TypeAdtApply(i => ADTData.empty(tailMapping.value(i)))
+    ): TypeAdtApply[A, AdtNatPositive[B, Tail], ST] = new TypeAdtApply(i => ADTData.empty(tailMapping.value(i)))
   }
 
   trait LowerLevelPoly {
-    private object failedValue extends TypeAdtApply[Any, AdtNatZero, ADTStatus.NotFinished](value = i => ADTData.zero)
-    private def failedValueImpl[T]: TypeAdtApply[T, AdtNatZero, ADTStatus.NotFinished] =
-      failedValue.asInstanceOf[TypeAdtApply[T, AdtNatZero, ADTStatus.NotFinished]]
-
-    implicit def adtFailedResult[I]: TypeAdtApply[I, AdtNatZero, ADTStatus.NotFinished] = failedValueImpl
+    implicit def adtFailedResult[I]: TypeAdtApply[I, AdtNatZero, ADTStatus.NotFinished] = Helper.failedValueImpl
   }
 
 }
