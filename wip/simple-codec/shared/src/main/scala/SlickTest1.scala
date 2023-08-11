feddd284dd0d615f5cd76ef6cc5bb54aabf122f0
漓江丽火 <909134790@qq.com>
2023-08-11 19:24:49 +0800

    Update test case.

--- wip/simple-codec/shared/src/main/scala/SlickTest1.scala
+++ wip/simple-codec/shared/src/main/scala/SlickTest1.scala
@@ -14,3 +14,26 @@ object Model1 {
   }
   val users = TableQuery[Users]
 }
+
+object Model2 {
+  case class UserAbs[F[_], U[_]](id: F[U[Int]], first: F[String], last: F[String])
+  type UserTableForInsert = UserAbs[RepCol, Option]
+  type UserTableForSelect = UserAbs[RepCol, Id]
+  type UserModelForInsert = UserAbs[Id, Option]
+  type UserModelForSelect = UserAbs[Id, Id]
+  type RepCol[T]          = Rep[T]
+  type Id[T]              = T
+
+  val userTable1: Table[_] => UserTableForSelect = { table =>
+    UserAbs[RepCol, Id](
+      id = table.column[Int]("id", table.O.PrimaryKey, table.O.AutoInc),
+      first = table.column[String]("first"),
+      last = table.column[String]("last")
+    )
+  }
+
+  val userTable2: Table[_] => UserTableForInsert = { table =>
+    val t = userTable1(table)
+    t.copy(id = t.id.?)
+  }
+}
