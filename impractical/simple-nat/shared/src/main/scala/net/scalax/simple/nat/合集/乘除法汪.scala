975e555085105b004bf1ae5a02b9a5f5f13111f1
漓江丽火 <909134790@qq.com>
2023-08-18 01:48:42 +0800

    New sample.

--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/合集/乘除法汪.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/合集/乘除法汪.scala
@@ -7,14 +7,14 @@ import scala.annotation.tailrec
 object 乘除法汪 {
 
   def main(arr: Array[String]): Unit = {
-    val num1: Int = 21
-    object num2 {
+    object num1 {
       val Parent: Int = 672
       val Sub: Int    = 4
     }
+    val num2: Int            = 21
     val num3: Int            = 8
-    val num1GHDMZSK: ghdmzsk = 统计.gen(positive = 合集.s1, zero = 合集.s4)(positiveLen = num1, zeroLen = 1)
-    val num2GHDMZSK: ghdmzsk = 统计.gen(positive = 合集.s2, zero = 合集.s3)(positiveLen = num2.Parent, zeroLen = num2.Sub)
+    val num1GHDMZSK: ghdmzsk = 统计.gen(positive = 合集.s2, zero = 合集.s3)(positiveLen = num1.Parent, zeroLen = num1.Sub)
+    val num2GHDMZSK: ghdmzsk = 统计.gen(positive = 合集.s1, zero = 合集.s4)(positiveLen = num2, zeroLen = 1)
     val num3GHDMZSK: ghdmzsk = 统计.gen(positive = 合集.s4, zero = 合集.s5)(positiveLen = num3, zeroLen = 1)
 
     val execResultForInput: ghdmzsk = num1GHDMZSK.inputGHDMZSK(num2GHDMZSK).inputGHDMZSK(num3GHDMZSK)
