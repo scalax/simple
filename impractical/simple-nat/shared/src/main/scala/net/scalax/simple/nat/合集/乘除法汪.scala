beb80d9e082ab928f6950e88a9db5230651f8aea
漓江丽火 <909134790@qq.com>
2023-08-18 02:11:14 +0800

    New sample.

--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/合集/乘除法汪.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/合集/乘除法汪.scala
@@ -8,18 +8,24 @@ object 乘除法汪 {
 
   def main(arr: Array[String]): Unit = {
     object num1 {
-      val Parent: Int = 2688
-      val Sub: Int    = 4
+      val Parent: Int = 42
+      val Sub: Int    = 16
     }
-    val num2: Int            = 21
-    val num3: Int            = 32
+    object num2 {
+      val Parent: Int = 105
+      val Sub: Int    = 5
+    }
+    object num3 {
+      val Parent: Int = 4
+      val Sub: Int    = 32
+    }
     val num1GHDMZSK: ghdmzsk = 统计.gen(positive = 合集.s2, zero = 合集.s3)(positiveLen = num1.Parent, zeroLen = num1.Sub)
-    val num2GHDMZSK: ghdmzsk = 统计.gen(positive = 合集.s1, zero = 合集.s4)(positiveLen = num2, zeroLen = 1)
-    val num3GHDMZSK: ghdmzsk = 统计.gen(positive = 合集.s4, zero = 合集.s5)(positiveLen = num3, zeroLen = 1)
+    val num2GHDMZSK: ghdmzsk = 统计.gen(positive = 合集.s1, zero = 合集.s4)(positiveLen = num2.Parent, zeroLen = num2.Sub)
+    val num3GHDMZSK: ghdmzsk = 统计.gen(positive = 合集.s4, zero = 合集.s5)(positiveLen = num3.Parent, zeroLen = num3.Sub)
 
     val execResultForInput: ghdmzsk = num1GHDMZSK.inputGHDMZSK(num2GHDMZSK).inputGHDMZSK(num3GHDMZSK)
 
-    统计.confirm(execResultForInput, brokeNum = 3000)
+    统计.confirm(execResultForInput)
   }
 
 }
