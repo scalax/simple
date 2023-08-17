01bac042aa2890f5f80a06095d44d5b1b5a7cbff
漓江丽火 <909134790@qq.com>
2023-08-17 12:34:19 +0800

    Typo.

--- wip/simple-codec/shared/src/main/scala/SlickTest1.scala
+++ wip/simple-codec/shared/src/main/scala/SlickTest1.scala
@@ -32,17 +32,15 @@ object Model2 {
 
   case class UserAbs[F[_], U[_]](id: F[U[Int]], first: F[String], last: F[String])
 
-  type Id[T]          = T
-  type StrAny[T]      = String
-  type ColOpt[T]      = Seq[ColumnOption[T]]
-  type ShapeF[T]      = Shape[_ <: FlatShapeLevel, Rep[T], T, _]
-  type ShapeValueF[T] = ShapedValue[Rep[T], T]
+  type Id[T]     = T
+  type StrAny[T] = String
+  type ColOpt[T] = Seq[ColumnOption[T]]
+  type ShapeF[T] = Shape[_ <: FlatShapeLevel, Rep[T], T, _]
 
   val helper: Helper[MySQLProfile] = new Helper(new U(slickProfile))
 
   def userTypedType[U[_]](implicit tt12: TypedType[U[Int]]): UserAbs[TypedType, U] =
     UserAbs[TypedType, U](implicitly, implicitly, implicitly)
-  def userShape[U[_]](implicit tt12: ShapeF[U[Int]]): UserAbs[ShapeF, U] = UserAbs[ShapeF, U](implicitly, implicitly, implicitly)
 
   def userNamed[U[_]]: UserAbs[StrAny, U] = UserAbs[StrAny, U](id = "id", first = "first", last = "last")
 
@@ -59,21 +57,7 @@ object Model2 {
     UserAbs[Rep, U](id = colN(l1.id, l2.id, l3.id), first = colN(l1.first, l2.first, l3.first), last = colN(l1.last, l2.last, l3.last))
   }
 
-  def userShapedValue[U[_]](implicit
-    tt1: TypedType[U[Int]],
-    tt12: ShapeF[U[Int]]
-  ): helper.TableHelper1[UserAbs[Id, U]] => UserAbs[ShapeValueF, U] = { colN =>
-    val rep1Impl = userRep[U]
-    val rep1     = rep1Impl(colN)
-    val shape1   = userShape[U]
-    UserAbs[ShapeValueF, U](
-      id = ShapedValue(rep1.id, shape1.id),
-      first = ShapedValue(rep1.first, shape1.first),
-      last = ShapedValue(rep1.last, shape1.last)
-    )
-  }
-
-  class TableUserAbs[F[_], U[_]](tag: Tag)(implicit tt: TypedType[U[Int]], s: ShapeF[U[Int]])
+  class TableUserAbs[U[_]](tag: Tag)(implicit tt: TypedType[U[Int]], s: ShapeF[U[Int]])
       extends helper.uAPI.profile.Table[UserAbs[Id, U]](tag, "users") {
     self =>
     def tableHelper = new helper.TableHelper1[UserAbs[Id, U]](self)
@@ -83,16 +67,7 @@ object Model2 {
       tImpl(tableHelper)
     }
 
-    def sv: UserAbs[ShapeValueF, U] = {
-      val v = userShapedValue[U]
-      v(tableHelper)
-    }
-
-    import slick.collection.heterogeneous._
-    import slick.collection.heterogeneous.syntax._
-
     override def * : ProvenShape[UserAbs[Id, U]] =
-      (sv.id :: sv.first :: sv.last :: HNil) <> (u => UserAbs.apply[Id, U](u.head, u.tail.head, u.tail.tail.head), (t: UserAbs[Id, U]) =>
-        Some(t.id :: t.first :: t.last :: HNil))
+      UserAbs.unapply[Rep, U](t).get <> ((UserAbs.apply[Id, U] _).tupled, UserAbs.unapply[Id, U] _)
   }
 }
