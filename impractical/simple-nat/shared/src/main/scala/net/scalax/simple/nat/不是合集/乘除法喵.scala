426390e8d6c4578a4c97c7cb90a1ab0bf5cf48f7
漓江丽火 <909134790@qq.com>
2023-08-18 23:52:24 +0800

    New sample.

--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/不是合集/乘除法喵.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/不是合集/乘除法喵.scala
@@ -19,14 +19,14 @@ object 乘除法喵 {
       val Parent: Int = 4
       val Sub: Int    = 32
     }
-    lazy val num1GHDMZSK: ghdmzsk      = 统计.gen(合集.s3, num1GHDMZSKOther)(num1.Parent)
-    lazy val num1GHDMZSKOther: ghdmzsk = 统计.gen(合集.s1, num1GHDMZSK)(num1.Sub)
+    lazy val num1GHDMZSK: ghdmzsk      = 统计.gen(合集.y.b, num1GHDMZSKOther)(num1.Parent)
+    lazy val num1GHDMZSKOther: ghdmzsk = 统计.gen(合集.x.a, num1GHDMZSK)(num1.Sub)
 
-    lazy val num2GHDMZSK: ghdmzsk      = 统计.gen(合集.s3, num2GHDMZSKOther)(num2.Parent)
-    lazy val num2GHDMZSKOther: ghdmzsk = 统计.gen(合集.s3, num2GHDMZSK)(num2.Sub, deep = 3)
+    lazy val num2GHDMZSK: ghdmzsk      = 统计.gen(合集.y.b, num2GHDMZSKOther)(num2.Parent)
+    lazy val num2GHDMZSKOther: ghdmzsk = 统计.gen(合集.y.a, num2GHDMZSK)(num2.Sub)
 
-    lazy val num3GHDMZSK: ghdmzsk      = 统计.gen(合集.s3, num3GHDMZSKOther)(num3.Parent, deep = 3)
-    lazy val num3GHDMZSKOther: ghdmzsk = 统计.gen(合集.s2, num3GHDMZSK)(num3.Sub)
+    lazy val num3GHDMZSK: ghdmzsk      = 统计.gen(合集.y.a, num3GHDMZSKOther)(num3.Parent)
+    lazy val num3GHDMZSKOther: ghdmzsk = 统计.gen(合集.x.b, num3GHDMZSK)(num3.Sub)
 
     val execResultForInput: ghdmzsk = num1GHDMZSK.inputGHDMZSK(num2GHDMZSK).inputGHDMZSK(num3GHDMZSK)
 
--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/不是合集/合集.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/不是合集/合集.scala
@@ -6,33 +6,37 @@ import scala.annotation.tailrec
 
 object 合集 {
 
-  val s1: ghdmzsk = new ghdmzsk {
-    override def inputGHDMZSK(n1: => ghdmzsk): ghdmzsk = new ghdmzsk {
-      override def inputGHDMZSK(n2: => ghdmzsk): ghdmzsk = new ghdmzsk {
-        override def inputGHDMZSK(n3: => ghdmzsk): ghdmzsk =
-          统计.confirm1.inputGHDMZSK(n1.inputGHDMZSK(n2).inputGHDMZSK(n3))
+  object x {
+    def a: ghdmzsk = new ghdmzsk {
+      override def inputGHDMZSK(n1: => ghdmzsk): ghdmzsk = new ghdmzsk {
+        override def inputGHDMZSK(n2: => ghdmzsk): ghdmzsk = new ghdmzsk {
+          override def inputGHDMZSK(n3: => ghdmzsk): ghdmzsk =
+            统计.confirm1.inputGHDMZSK(n1.inputGHDMZSK(n2).inputGHDMZSK(n3))
+        }
+      }
+    }
+    def b: ghdmzsk = new ghdmzsk {
+      override def inputGHDMZSK(n1: => ghdmzsk): ghdmzsk = new ghdmzsk {
+        override def inputGHDMZSK(n2: => ghdmzsk): ghdmzsk = new ghdmzsk {
+          override def inputGHDMZSK(n3: => ghdmzsk): ghdmzsk =
+            统计.confirm2.inputGHDMZSK(n1.inputGHDMZSK(n2).inputGHDMZSK(n3))
+        }
       }
     }
   }
-  val s2: ghdmzsk = new ghdmzsk {
-    override def inputGHDMZSK(n1: => ghdmzsk): ghdmzsk = new ghdmzsk {
-      override def inputGHDMZSK(n2: => ghdmzsk): ghdmzsk = new ghdmzsk {
-        override def inputGHDMZSK(n3: => ghdmzsk): ghdmzsk =
-          统计.confirm2.inputGHDMZSK(n1.inputGHDMZSK(n2).inputGHDMZSK(n3))
+  object y {
+    val a: ghdmzsk = new ghdmzsk {
+      override def inputGHDMZSK(n3: => ghdmzsk): ghdmzsk = new ghdmzsk {
+        override def inputGHDMZSK(n2: => ghdmzsk): ghdmzsk = new ghdmzsk {
+          override def inputGHDMZSK(n1: => ghdmzsk): ghdmzsk = n1.inputGHDMZSK(n2).inputGHDMZSK(n3)
+        }
       }
     }
-  }
-  val s4: ghdmzsk = new ghdmzsk {
-    override def inputGHDMZSK(n1: => ghdmzsk): ghdmzsk = new ghdmzsk {
+    val b: ghdmzsk = new ghdmzsk {
       override def inputGHDMZSK(n2: => ghdmzsk): ghdmzsk = new ghdmzsk {
-        override def inputGHDMZSK(n3: => ghdmzsk): ghdmzsk = n1.inputGHDMZSK(n2).inputGHDMZSK(n3)
-      }
-    }
-  }
-  val s3: ghdmzsk = new ghdmzsk {
-    override def inputGHDMZSK(n1: => ghdmzsk): ghdmzsk = new ghdmzsk {
-      override def inputGHDMZSK(n2: => ghdmzsk): ghdmzsk = new ghdmzsk {
-        override def inputGHDMZSK(n3: => ghdmzsk): ghdmzsk = n2.inputGHDMZSK(n1).inputGHDMZSK(n3)
+        override def inputGHDMZSK(n1: => ghdmzsk): ghdmzsk = new ghdmzsk {
+          override def inputGHDMZSK(n3: => ghdmzsk): ghdmzsk = n1.inputGHDMZSK(n2).inputGHDMZSK(n3)
+        }
       }
     }
   }
