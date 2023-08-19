6e5b5344dbee38667f93aebcd49752588e884f3f
漓江丽火 <909134790@qq.com>
2023-08-20 01:01:30 +0800

    New sample.

--- wip/simple-codec/shared/src/main/scala-2/net/scalax/simple/codec/ModelNameImplicitImpl.scala
+++ wip/simple-codec/shared/src/main/scala-2/net/scalax/simple/codec/ModelNameImplicitImpl.scala
@@ -2,6 +2,10 @@ package net.scalax.simple.codec
 
 import slick.jdbc.JdbcProfile
 
-class U[V <: JdbcProfile](val profile: V) {
+class SlickCompatAlias[V <: JdbcProfile](val profile: V) {
   type ColumnOptions = profile.ColumnOptions
+}
+
+object SlickCompatAlias {
+  def build[V <: JdbcProfile](implicit p: V): SlickCompatAlias[V] = new SlickCompatAlias(profile = p)
 }
