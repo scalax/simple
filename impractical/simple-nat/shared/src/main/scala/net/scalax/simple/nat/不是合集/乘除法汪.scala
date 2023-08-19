caae26234e6aaa6a561ed042c849214e4b261d11
漓江丽火 <909134790@qq.com>
2023-08-19 12:59:59 +0800

    New sample.

--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/不是合集/乘除法汪.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/不是合集/乘除法汪.scala
@@ -12,18 +12,18 @@ object 乘除法汪 {
       val Sub: Int    = 5
     }
     object num2 {
-      val Parent: Int =42
-      val Sub: Int    =16
+      val Parent: Int = 42
+      val Sub: Int    = 16
     }
     object num3 {
       val Parent: Int = 4
       val Sub: Int    = 32
     }
     lazy val num1GHDMZSK: ghdmzsk      = 统计.gen(合集.y.b, num1GHDMZSKOther)(num1.Parent)
-    lazy val num1GHDMZSKOther: ghdmzsk = 统计.gen(合集.x.a, num1GHDMZSK)(num1.Sub)
+    lazy val num1GHDMZSKOther: ghdmzsk = 统计.gen(合集.y.a, num1GHDMZSK)(num1.Sub)
 
     lazy val num2GHDMZSK: ghdmzsk      = 统计.gen(合集.y.b, num2GHDMZSKOther)(num2.Parent)
-    lazy val num2GHDMZSKOther: ghdmzsk = 统计.gen(合集.y.a, num2GHDMZSK)(num2.Sub)
+    lazy val num2GHDMZSKOther: ghdmzsk = 统计.gen(合集.x.a, num2GHDMZSK)(num2.Sub)
 
     lazy val num3GHDMZSK: ghdmzsk      = 统计.gen(合集.y.a, num3GHDMZSKOther)(num3.Parent)
     lazy val num3GHDMZSKOther: ghdmzsk = 统计.gen(合集.x.b, num3GHDMZSK)(num3.Sub)
