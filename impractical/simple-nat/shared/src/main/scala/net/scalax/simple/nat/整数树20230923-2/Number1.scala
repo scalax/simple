66d9c17fbc34885226bc16de239f202142d2aa52
漓江丽火 <909134790@qq.com>
2023-09-25 03:37:29 +0800

    add temp. More generic.

--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/整数树20230923-2/Number1.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/整数树20230923-2/Number1.scala
@@ -5,43 +5,29 @@ import net.scalax.simple.nat.合集1.合集
 object Number1 {
 
   // ========= ResultA
-  class NumACount(next: () => NumACount) extends 合集.NumCount(next)
-
-  case class NumACountPositive(next: () => NumACount, precursor: NumBCount) extends NumACount(next)
-
-  private class NumACountZeroImpl(next: () => NumACountZeroImpl) extends NumACount(next)
-  private lazy val NumACountZeroImpl: NumACountZeroImpl = new NumACountZeroImpl(next = () => NumACountZeroImpl)
-  val NumACountZero: NumACount                          = NumACountZeroImpl
+  class NumACount(next: () => NumACount)                                          extends 合集.NumCount(next)
+  case class NumACountPositive(next: () => NumACount, precursor: () => NumBCount) extends NumACount(next)
 
   // ========= ResultB
-  class NumBCount(next: () => NumBCount) extends 合集.NumCount(next)
-
-  case class NumBCountPositive(next: () => NumBCount, precursor: NumACount) extends NumBCount(next)
-
-  private class NumBCountZeroImpl(next: () => NumBCountZeroImpl) extends NumBCount(next)
-  private lazy val NumBCountZeroImpl: NumBCountZeroImpl = new NumBCountZeroImpl(next = () => NumBCountZeroImpl)
-  val NumBCountZero: NumBCount                          = NumBCountZeroImpl
+  class NumBCount(next: () => NumBCount)                                          extends 合集.NumCount(next)
+  case class NumBCountPositive(next: () => NumBCount, precursor: () => NumACount) extends NumBCount(next)
 
   // ========== NumberA
   abstract class NumAExecute(next: () => NumAExecute) extends NumACount(next = next) {
     def method1(numA: NumAExecute, numB: NumBExecute): NumACount
   }
 
-  case class NumAExecutePositive(next: () => NumAExecute, precursor: NumBExecute) extends NumAExecute(next) {
+  case class NumAExecutePositive(next: () => NumAExecute, precursor: () => NumBExecute) extends NumAExecute(next) {
     override def method1(numA1: NumAExecute, numB1: NumBExecute): NumACount =
-      NumACountPositive(next = () => next().method1(numA1, NumBExecuteZeroImpl), precursor = precursor.method2(numB1, NumAExecuteZeroImpl))
+      NumACountPositive(
+        next = () => next().method1(numA1, NumBExecuteZeroImpl),
+        precursor = () => precursor().method2(numB1, NumAExecuteZeroImpl)
+      )
   }
 
   private class NumAExecuteZeroImpl(next: () => NumAExecuteZeroImpl) extends NumAExecute(next) {
     override def method1(numA1: NumAExecute, numB1: NumBExecute): NumACount = {
-      val needEnd1: Boolean = next() == NumAExecuteZeroImpl
-      val needEnd2: Boolean = numA1 == NumAExecuteZeroImpl
-      val needEnd3: Boolean = numB1 == NumBExecuteZeroImpl
-      if (needEnd1 && needEnd2 && needEnd3) {
-        NumACountZero
-      } else {
-        numA1.method1(next(), numB1)
-      }
+      numA1.method1(next(), numB1)
     }
   }
   private lazy val NumAExecuteZeroImpl: NumAExecuteZeroImpl = new NumAExecuteZeroImpl(next = () => NumAExecuteZeroImpl)
@@ -52,21 +38,17 @@ object Number1 {
     def method2(numB: NumBExecute, numA: NumAExecute): NumBCount
   }
 
-  case class NumBExecutePositive(next: () => NumBExecute, precursor: NumAExecute) extends NumBExecute(next) {
+  case class NumBExecutePositive(next: () => NumBExecute, precursor: () => NumAExecute) extends NumBExecute(next) {
     override def method2(numB1: NumBExecute, numA1: NumAExecute): NumBCount =
-      NumBCountPositive(next = () => next().method2(numB1, NumAExecuteZeroImpl), precursor = precursor.method1(numA1, NumBExecuteZeroImpl))
+      NumBCountPositive(
+        next = () => next().method2(numB1, NumAExecuteZeroImpl),
+        precursor = () => precursor().method1(numA1, NumBExecuteZeroImpl)
+      )
   }
 
   private class NumBExecuteZeroImpl(next: () => NumBExecuteZeroImpl) extends NumBExecute(next) {
     override def method2(numB1: NumBExecute, numA1: NumAExecute): NumBCount = {
-      val needEnd1: Boolean = next() == NumBExecuteZeroImpl
-      val needEnd2: Boolean = numB1 == NumBExecuteZeroImpl
-      val needEnd3: Boolean = numA1 == NumAExecuteZeroImpl
-      if (needEnd1 && needEnd2 && needEnd3) {
-        NumBCountZero
-      } else {
-        numB1.method2(next(), numA1)
-      }
+      numB1.method2(next(), numA1)
     }
   }
   private lazy val NumBExecuteZeroImpl: NumBExecuteZeroImpl = new NumBExecuteZeroImpl(next = () => NumBExecuteZeroImpl)
--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/整数树20230923-2/Number2.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/整数树20230923-2/Number2.scala
@@ -4,37 +4,58 @@ object NENE喵A呜哇 {
 
   import Number1._
 
-  def countExecuteA(numImplAbs: NumAExecute): Int = numImplAbs match {
-    case NumAExecutePositive(numA, numB) => countExecuteA(numA()) + countExecuteB(numB) + 1
+  def countExecuteA(numImplAbs: () => NumAExecute): Int = numImplAbs() match {
+    case NumAExecutePositive(numA, numB) => countExecuteA(numA) + countExecuteB(numB) + 1
     case NumAExecuteZero                 => 0
   }
 
-  def countExecuteB(numImplAbs: NumBExecute): Int = numImplAbs match {
-    case NumBExecutePositive(numB, numA) => countExecuteA(numA) + countExecuteB(numB()) - 1
+  def countExecuteB(numImplAbs: () => NumBExecute): Int = numImplAbs() match {
+    case NumBExecutePositive(numB, numA) => countExecuteA(numA) + countExecuteB(numB) - 1
     case NumBExecuteZero                 => 0
   }
 
-  def countResultA(numImplAbs: NumACount): Int = numImplAbs match {
-    case NumACountPositive(numA, numB) => countResultA(numA()) + countResultB(numB) + 1
-    case NumACountZero                 => 0
+  def countResultA(numImplAbs: () => NumACount): Int = {
+    val tempInstance =
+      try
+        Option(numImplAbs())
+      catch {
+        case e: StackOverflowError => Option.empty
+      }
+
+    tempInstance
+      .map { case NumACountPositive(numA, numB) =>
+        countResultA(numA) + countResultB(numB) + 1
+      }
+      .getOrElse(0)
   }
 
-  def countResultB(numImplAbs: NumBCount): Int = numImplAbs match {
-    case NumBCountPositive(numB, numA) => countResultA(numA) + countResultB(numB()) - 1
-    case NumBCountZero                 => 0
+  def countResultB(numImplAbs: () => NumBCount): Int = {
+    val tempInstance =
+      try
+        Option(numImplAbs())
+      catch {
+        case e: StackOverflowError => Option.empty
+      }
+
+    tempInstance
+      .map { case NumBCountPositive(numB, numA) =>
+        countResultA(numA) + countResultB(numB) - 1
+      }
+      .getOrElse(0)
   }
 
   def main(arr: Array[String]): Unit = {
     val num1 = NumAExecutePositive(
       () => NumAExecuteZero,
-      NumBExecutePositive(
-        () =>
-          NumBExecutePositive(
-            () => NumBExecutePositive(() => NumBExecutePositive(() => NumBExecuteZero, NumAExecuteZero), NumAExecuteZero),
-            NumAExecuteZero
-          ),
-        NumAExecutePositive(() => NumAExecuteZero, NumBExecutePositive(() => NumBExecuteZero, NumAExecuteZero))
-      )
+      () =>
+        NumBExecutePositive(
+          () =>
+            NumBExecutePositive(
+              () => NumBExecutePositive(() => NumBExecutePositive(() => NumBExecuteZero, () => NumAExecuteZero), () => NumAExecuteZero),
+              () => NumAExecuteZero
+            ),
+          () => NumAExecutePositive(() => NumAExecuteZero, () => NumBExecutePositive(() => NumBExecuteZero, () => NumAExecuteZero))
+        )
     )
     val num2 =
       NumAExecutePositive(
@@ -51,49 +72,64 @@ object NENE喵A呜哇 {
                             () =>
                               NumAExecutePositive(
                                 () =>
-                                  NumAExecutePositive(() => NumAExecuteZero, NumBExecutePositive(() => NumBExecuteZero, NumAExecuteZero)),
-                                NumBExecutePositive(() => NumBExecuteZero, NumAExecutePositive(() => NumAExecuteZero, NumBExecuteZero))
+                                  NumAExecutePositive(
+                                    () => NumAExecuteZero,
+                                    () => NumBExecutePositive(() => NumBExecuteZero, () => NumAExecuteZero)
+                                  ),
+                                () =>
+                                  NumBExecutePositive(
+                                    () => NumBExecuteZero,
+                                    () => NumAExecutePositive(() => NumAExecuteZero, () => NumBExecuteZero)
+                                  )
                               ),
-                            NumBExecuteZero
+                            () => NumBExecuteZero
                           ),
-                        NumBExecuteZero
+                        () => NumBExecuteZero
                       ),
-                    NumBExecuteZero
+                    () => NumBExecuteZero
                   ),
-                NumBExecuteZero
+                () => NumBExecuteZero
               ),
-            NumBExecuteZero
+            () => NumBExecuteZero
           ),
-        NumBExecuteZero
+        () => NumBExecuteZero
       )
     val num3 =
       NumBExecutePositive(
-        () => NumBExecutePositive(() => NumBExecutePositive(() => NumBExecuteZero, NumAExecuteZero), NumAExecuteZero),
-        NumAExecutePositive(() => num1, NumBExecuteZero)
+        () => NumBExecutePositive(() => NumBExecutePositive(() => NumBExecuteZero, () => NumAExecuteZero), () => NumAExecuteZero),
+        () => NumAExecutePositive(() => num1, () => NumBExecuteZero)
       )
     val num4 =
       NumBExecutePositive(
-        () => NumBExecutePositive(() => NumBExecutePositive(() => NumBExecutePositive(() => NumBExecuteZero, num2), num1), NumAExecuteZero),
-        NumAExecutePositive(
-          () => num1,
+        () =>
           NumBExecutePositive(
+            () => NumBExecutePositive(() => NumBExecutePositive(() => NumBExecuteZero, () => num2), () => num1),
+            () => NumAExecuteZero
+          ),
+        () =>
+          NumAExecutePositive(
+            () => num1,
             () =>
               NumBExecutePositive(
-                () => NumBExecutePositive(() => NumBExecutePositive(() => num3, NumAExecuteZero), NumAExecuteZero),
-                NumAExecuteZero
-              ),
-            NumAExecuteZero
+                () =>
+                  NumBExecutePositive(
+                    () => NumBExecutePositive(() => NumBExecutePositive(() => num3, () => NumAExecuteZero), () => NumAExecuteZero),
+                    () => NumAExecuteZero
+                  ),
+                () => NumAExecuteZero
+              )
           )
-        )
       )
 
-    println("Need-1: " + countExecuteA(num1))
-    println("Need-2: " + countExecuteA(num2))
-    println("Not-Need-1: " + countExecuteB(num3))
-    println("Need-3: " + countExecuteB(num4))
-    println("Result-4: " + countResultA(num1.method1(num2, num4)))
+    println("Need-1: " + countExecuteA(() => num1))
+    println("Need-2: " + countExecuteA(() => num2))
+    println("Not-Need-1: " + countExecuteB(() => num3))
+    println("Need-3: " + countExecuteB(() => num4))
+    println("Result-4: " + countResultA(() => num1.method1(num2, num4)))
 
-    assert(countExecuteA(num1) + countExecuteA(num2) + countExecuteB(num4) == countResultA(num1.method1(num2, num4)))
+    assert(
+      countExecuteA(() => num1) + countExecuteA(() => num2) + countExecuteB(() => num4) == countResultA(() => num1.method1(num2, num4))
+    )
   }
 
 }
