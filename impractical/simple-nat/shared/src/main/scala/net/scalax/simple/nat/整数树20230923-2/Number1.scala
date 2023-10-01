36b442065f6ceca6106d089ad9f1742b98334bbc
漓江丽火 <909134790@qq.com>
2023-10-02 03:07:00 +0800

    Update doc.

--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/整数树20230923-2/Number1.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/整数树20230923-2/Number1.scala
@@ -7,31 +7,31 @@ object Number1 {
   case class NumBCount(next: () => NumBCount, precursor: () => NumACount)
 
   // ========== NumberA
-  class NumAExecute(next: () => NumAExecute) {
+  class NumAExecute(next: () => NumAExecute, precursor: () => NumBExecute) extends NumACount(next = next, precursor = precursor) {
     def method1(numA: NumAExecute, numB: NumBExecute): NumACount = numA.method1(next(), numB)
   }
 
-  case class NumAExecutePositive(next: () => NumAExecute, precursor: () => NumBExecute) extends NumAExecute(next) {
+  class NumAExecutePositive(next: () => NumAExecute, precursor: () => NumBExecute) extends NumAExecute(next = next, precursor = precursor) {
     override def method1(numA: NumAExecute, numB: NumBExecute): NumACount = NumACount(
       next = () => next().method1(numA, NumBExecuteZero),
       precursor = () => precursor().method2(numB, NumAExecuteZero)
     )
   }
 
-  lazy val NumAExecuteZero: NumAExecute = new NumAExecute(next = () => NumAExecuteZero)
+  lazy val NumAExecuteZero: NumAExecute = new NumAExecute(next = () => NumAExecuteZero, precursor = () => NumBExecuteZero)
 
   // ========== NumberB
-  class NumBExecute(next: () => NumBExecute) {
+  class NumBExecute(next: () => NumBExecute, precursor: () => NumAExecute) extends NumBCount(next = next, precursor = precursor) {
     def method2(numB: NumBExecute, numA: NumAExecute): NumBCount = numB.method2(next(), numA)
   }
 
-  case class NumBExecutePositive(next: () => NumBExecute, precursor: () => NumAExecute) extends NumBExecute(next) {
+  class NumBExecutePositive(next: () => NumBExecute, precursor: () => NumAExecute) extends NumBExecute(next = next, precursor = precursor) {
     override def method2(numB: NumBExecute, numA: NumAExecute): NumBCount = NumBCount(
       next = () => next().method2(numB, NumAExecuteZero),
       precursor = () => precursor().method1(numA, NumBExecuteZero)
     )
   }
 
-  val NumBExecuteZero: NumBExecute = new NumBExecute(() => NumBExecuteZero)
+  lazy val NumBExecuteZero: NumBExecute = new NumBExecute(() => NumBExecuteZero, precursor = () => NumAExecuteZero)
 
 }
