414ccc3eb31ede3f7801a26e21722fc95f40322e
漓江丽火 <909134790@qq.com>
2023-10-10 04:21:47 +0800

    pythagoras pythagoras pythagoras pythagoras.

--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/乘除法20231010/乘除法喵.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/乘除法20231010/乘除法喵.scala
@@ -1,10 +1,10 @@
-package net.scalax.simple.nat.`乘除法20231005`
+package net.scalax.simple.nat.乘除法20231010
 
 import net.scalax.simple.adt.{TypeAdt => Adt}
 
 object 乘除法喵 {
 
-  def main1(arr: Array[String]): Unit = {
+  def main(arr: Array[String]): Unit = {
     object num1 {
       val Parent: Int = 24
       val Sub: Int    = 547
@@ -17,21 +17,22 @@ object 乘除法喵 {
       val Parent: Int = 57
       val Sub: Int    = 27
     }
-    lazy val num1GHDMZSK: 合集.InputNum = 统计.gen(leftBuilder = t => new 合集.InputNum1(t), rightBuilder = t => new 合集.InputNum4(t))(
+    lazy val num1GHDMZSK: 合集.InputNum = 统计.gen(leftBuilder = t => new 合集.InputNum1(t), rightBuilder = t => new 合集.InputNum2(t))(
       leftLength = num1.Parent,
       rightLength = num1.Sub
     )(num1GHDMZSK)
-    lazy val num2GHDMZSK: 合集.InputNum = 统计.gen(leftBuilder = t => new 合集.InputNum5(t), rightBuilder = t => new 合集.InputNum6(t))(
+    lazy val num2GHDMZSK: 合集.InputNum = 统计.gen(leftBuilder = t => new 合集.InputNum3(t), rightBuilder = t => new 合集.InputNum4(t))(
       leftLength = num2.Parent,
       rightLength = num2.Sub
     )(num2GHDMZSK)
-    lazy val num3GHDMZSK: 合集.InputNum = 统计.gen(leftBuilder = t => new 合集.InputNum3(t), rightBuilder = t => new 合集.InputNum2(t))(
+    lazy val num3GHDMZSK: 合集.InputNum = 统计.gen(leftBuilder = t => new 合集.InputNum5(t), rightBuilder = t => new 合集.InputNum6(t))(
       leftLength = num3.Parent,
       rightLength = num3.Sub
     )(num3GHDMZSK)
 
+    val numMapInput: Map[Int, 合集.InputNum] = Map(1 -> num2GHDMZSK, 3 -> num3GHDMZSK)
     val execResultForInput: Adt.CoProduct2[合集.NumCountLeft, 合集.NumCountRight] =
-      num1GHDMZSK.fold(a => a.input(num2GHDMZSK, num3GHDMZSK), a => a.input(num2GHDMZSK, num3GHDMZSK))
+      num1GHDMZSK.fold(a => a.input(numMapInput), a => a.input(numMapInput))
 
     val bigDecimal1: BigDecimal = BigDecimal(num1.Sub) / BigDecimal(num1.Parent)
     val bigDecimal2: BigDecimal = BigDecimal(num2.Sub) / BigDecimal(num2.Parent)
---
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/乘除法20231010/合集.scala
@@ -0,0 +1,55 @@
+package net.scalax.simple.nat.乘除法20231010
+
+import net.scalax.simple.adt.{TypeAdt => Adt}
+
+object 合集 {
+
+  val setter1 = Adt.CoProducts2[NumCountLeft, NumCountRight]
+  val setter2 = Adt.CoProducts2[InputNumLeft, InputNumRight]
+
+  // Result
+  case class NumCountLeft(tail: () => Adt.CoProduct2[NumCountLeft, NumCountRight])
+  case class NumCountRight(tail: () => Adt.CoProduct2[NumCountLeft, NumCountRight])
+
+  abstract class InputNumLeft(num1: () => Adt.CoProduct2[InputNumLeft, InputNumRight]) extends NumCountLeft(num1) {
+    def input(other: Map[Int, Adt.CoProduct2[InputNumLeft, InputNumRight]]): Adt.CoProduct2[NumCountLeft, NumCountRight]
+  }
+  abstract class InputNumRight(num1: () => Adt.CoProduct2[InputNumLeft, InputNumRight]) extends NumCountRight(num1) {
+    def input(other: Map[Int, Adt.CoProduct2[InputNumLeft, InputNumRight]]): Adt.CoProduct2[NumCountLeft, NumCountRight]
+  }
+
+  class InputNum1(num1: () => Adt.CoProduct2[InputNumLeft, InputNumRight]) extends InputNumLeft(num1) {
+    def input(other: Map[Int, Adt.CoProduct2[InputNumLeft, InputNumRight]]): Adt.CoProduct2[NumCountLeft, NumCountRight] = setter1(
+      NumCountLeft(() => num1().fold(a => a.input(other), a => a.input(other)))
+    )
+  }
+
+  class InputNum2(num1: () => Adt.CoProduct2[InputNumLeft, InputNumRight]) extends InputNumRight(num1) {
+    def input(other: Map[Int, Adt.CoProduct2[InputNumLeft, InputNumRight]]): Adt.CoProduct2[NumCountLeft, NumCountRight] =
+      other(2).fold(a => a.input(other + ((1, num1()))), a => a.input(other + ((1, num1()))))
+  }
+
+  class InputNum3(num1: () => Adt.CoProduct2[InputNumLeft, InputNumRight]) extends InputNumLeft(num1) {
+    def input(other: Map[Int, Adt.CoProduct2[InputNumLeft, InputNumRight]]): Adt.CoProduct2[NumCountLeft, NumCountRight] =
+      other(2).fold(a => a.input(other + ((1, num1()))), a => a.input(other + ((1, num1()))))
+  }
+
+  class InputNum4(num1: () => Adt.CoProduct2[InputNumLeft, InputNumRight]) extends InputNumRight(num1) {
+    def input(other: Map[Int, Adt.CoProduct2[InputNumLeft, InputNumRight]]): Adt.CoProduct2[NumCountLeft, NumCountRight] =
+      other(3).fold(a => a.input(other + ((1, num1()))), a => a.input(other + ((1, num1()))))
+  }
+
+  class InputNum5(num1: () => Adt.CoProduct2[InputNumLeft, InputNumRight]) extends InputNumLeft(num1) {
+    def input(other: Map[Int, Adt.CoProduct2[InputNumLeft, InputNumRight]]): Adt.CoProduct2[NumCountLeft, NumCountRight] =
+      other(3).fold(a => a.input(other + ((1, num1()))), a => a.input(other + ((1, num1()))))
+  }
+
+  class InputNum6(num1: () => Adt.CoProduct2[InputNumLeft, InputNumRight]) extends InputNumRight(num1) {
+    def input(other: Map[Int, Adt.CoProduct2[InputNumLeft, InputNumRight]]): Adt.CoProduct2[NumCountLeft, NumCountRight] = setter1(
+      NumCountRight(() => num1().fold(a => a.input(other), a => a.input(other)))
+    )
+  }
+
+  type InputNum = Adt.CoProduct2[InputNumLeft, InputNumRight]
+
+}
