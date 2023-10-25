e5b6f30e3e879271366e3fe4151b3dbeb9c6a4ea
漓江丽火 <909134790@qq.com>
2023-10-26 02:13:47 +0800

    Add AnyVal and 2.11 compat.

--- main/test/test-common/shared/src/main/scala-2.11/net/scalax/simple/test/Compat.scala
+++ main/test/test-common/shared/src/main/scala-2.11/net/scalax/simple/test/Compat.scala
@@ -4,7 +4,7 @@ import zio.test.TestResult
 
 object SimpleCompat {
 
-  implicit class bb(t: TestResult.type) extends AnyVal{
+  implicit class bb(val t: TestResult.type) extends AnyVal {
     @inline def allSuccesses(assert: TestResult, asserts: TestResult*): TestResult = {
       val assertsSeq = assert +: asserts
       t.all(assertsSeq: _*)
