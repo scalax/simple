416d6b476879dd7505a402fed352b63708e3ea19
漓江丽火 <909134790@qq.com>
2024-01-18 05:29:28 +0800

    pythagoras.

--- wip/simple-codec/shared/src/main/scala-2/net/scalax/simple/codec/decode_projection/DecodeProjection1.scala
+++ wip/simple-codec/shared/src/main/scala-2/net/scalax/simple/codec/decode_projection/DecodeProjection1.scala
@@ -7,14 +7,15 @@ trait DataHList {
   type DataHead
   type DataTail <: DataHList
   type ToHList <: HList
+  type AppendF[F[_[_]], U <: TypeHList[F]] <: DataHList with TypeHList[F]
 }
 
-trait MergeData[F[_[_]], TH <: TypeHList[F], DT <: DataHList] extends DataHList with TypeHList[F] {
+/*trait MergeData[F[_[_]], TH <: TypeHList[F], DT <: DataHList] extends DataHList with TypeHList[F] {
   override type DataHead = TH#Head[DT#DataHead]
   override type Head[T]  = TH#Head[T]
   override type DataTail = MergeData[F, TH, DT#DataTail]
   override type Tail     = MergeData[F, TH#Tail, DT]
-  // override type ToHList  = TH#Head[DT#DataHead] :: DataTail#ToHList
+   override type ToHList  = TH#Head[DT#DataHead] :: DataTail#ToHList
 }
 
 trait MergeDataZero[F[_[_]], TH <: TypeHList[F]] extends MergeData[F, TH, DataHListZero] {
@@ -23,7 +24,7 @@ trait MergeDataZero[F[_[_]], TH <: TypeHList[F]] extends MergeData[F, TH, DataHL
   override type DataTail = MergeData[F, TH, DataHListZero]
   override type Tail     = MergeData[F, TH#Tail, DataHListZero]
   override type ToHList  = HNil
-}
+}*/
 
 trait DataHListZero extends DataHList {
   override type DataHead = ZeroInstance
