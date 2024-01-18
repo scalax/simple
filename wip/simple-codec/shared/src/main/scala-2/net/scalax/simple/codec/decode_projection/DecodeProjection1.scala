b9978f68fc53fe8bed501c12d451467901df27c2
漓江丽火 <909134790@qq.com>
2024-01-19 07:36:44 +0800

    pythagoras.

--- wip/simple-codec/shared/src/main/scala-2/net/scalax/simple/codec/decode_projection/DecodeProjection1.scala
+++ wip/simple-codec/shared/src/main/scala-2/net/scalax/simple/codec/decode_projection/DecodeProjection1.scala
@@ -4,40 +4,77 @@ package decode.projection
 import shapeless._
 
 trait DataHList {
-  type DataHead
   type DataTail <: DataHList
   type ToHList <: HList
-  type UpToF[F[_]] <: DataHListF[DataHead, F, DataTail]
+  type UpToF[F[_]] <: DataHListF[F]
 }
 
 trait DataHListPositive[H, T <: DataHList] extends DataHList {
-  override type DataHead    = H
   override type DataTail    = T
   override type ToHList     = H :: T#ToHList
-  override type UpToF[F[_]] = DataHListFPositive[DataHead, F, DataTail]
+  override type UpToF[F[_]] = DataHListFPositive[H, F, DataTail]
 }
 
 trait DataHListZero extends DataHList {
-  override type DataHead    = ZeroInstance
-  override type DataTail    = DataHListZero
+  override type DataTail <: DataHListZero
   override type ToHList     = HNil
   override type UpToF[F[_]] = DataHListFZero[F]
 }
 
-trait DataHListF[H, F[_], Tail <: DataHList] extends DataHList {
-  override type DataHead = F[H]
-  override type DataTail <: DataHListF[Tail#DataHead, F, Tail]
+trait DataHListF[F[_]] extends DataHList {
+  override type DataTail <: DataHListF[F]
   override type ToHList <: HList
 }
 
-trait DataHListFPositive[H, F[_], Tail <: DataHList] extends DataHListF[H, F, Tail] {
-  override type DataHead = F[H]
-  override type DataTail = DataHListF[Tail#DataHead, F, Tail]
-  override type ToHList  = DataHead :: DataTail#ToHList
-}
+trait DataHListFPositive[H, F[_], Tail <: DataHList] extends DataHListF[F] with DataHListPositive[F[H], Tail#UpToF[F]]
 
-trait DataHListFZero[F[_]] extends DataHListF[ZeroInstance, F, DataHListZero] {
-  override type DataHead = F[ZeroInstance]
+trait DataHListFZero[F[_]] extends DataHListF[F] with DataHListZero {
   override type DataTail = DataHListFZero[F]
   override type ToHList  = HNil
+}
+
+object AAAA {
+
+  class Tag[T]
+
+  type HList1 = DataHListPositive[String, DataHListPositive[
+    List[String],
+    DataHListPositive[List[Int], DataHListPositive[Long, DataHListZero]]
+  ]]
+
+  locally {
+    val data1: Tag[HList1#ToHList] = new Tag[HList1#ToHList]
+
+    val data2: Tag[String :: List[String] :: List[Int] :: Long :: HNil] = data1
+  }
+
+  locally {
+    val data1: Tag[HList1#UpToF[List]#ToHList] =
+      new Tag[HList1#UpToF[List]#ToHList]
+
+    val data2: Tag[
+      List[String] :: List[List[String]] :: List[List[Int]] :: List[Long] :: HNil
+    ] = data1
+  }
+
+  locally {
+    trait Param123[T]
+
+    val data1: Tag[HList1#UpToF[Param123]#ToHList] =
+      new Tag[HList1#UpToF[Param123]#ToHList]
+
+    val data2: Tag[
+      Param123[String] :: Param123[List[String]] ::
+        Param123[
+          List[Int]
+        ] :: Param123[Long] :: HNil
+    ] = data1
+
+    val data3: Tag[HList1#UpToF[Param123]#UpToF[Param123]#ToHList] = new Tag[HList1#UpToF[Param123]#UpToF[Param123]#ToHList]
+
+    val data4: Tag[
+      Param123[Param123[String]] :: Param123[Param123[List[String]]] :: Param123[Param123[List[Int]]] :: Param123[Param123[Long]] :: HNil
+    ] = data3
+
+  }
 }
