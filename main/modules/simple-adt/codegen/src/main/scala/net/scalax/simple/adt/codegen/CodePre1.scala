c071055846877ad00b5c1a591e3417d45b69b24d
漓江丽火 <909134790@qq.com>
2024-02-24 04:00:29 +0800

    op pyt pythagoras.

---
+++ main/modules/simple-adt/codegen/src/main/scala/net/scalax/simple/adt/codegen/CodePre1.scala
@@ -0,0 +1,17 @@
+package net.scalax.simple.adt.codegen
+
+import java.io.PrintWriter
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Paths}
+import scala.util.Using
+
+object CodePre1:
+
+  val text1: String =s"""
+trait ADTPassedFunctionImpl {
+//
+//
+}
+""".stripMargin
+
+end CodePre1
