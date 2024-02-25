7ee7de457a51b49bd9b0d9bfa742d3ebc6b8f906
漓江丽火 <909134790@qq.com>
2024-02-25 17:58:59 +0800

    pythagoras.

--- main/modules/simple-adt/codegen/src/main/scala/net/scalax/simple/adt/codegen/CodePre1.scala
+++ main/modules/simple-adt/codegen/src/main/scala/net/scalax/simple/adt/codegen/CodePre1.scala
@@ -1,16 +1,23 @@
 package net.scalax.simple.adt.codegen
 
-import java.io.PrintWriter
-import java.nio.charset.StandardCharsets
-import java.nio.file.{Files, Paths}
-import scala.util.Using
-
 object CodePre1:
 
-  val text1: String =s"""
-trait ADTPassedFunctionImpl {
-//
-//
+  def repeat(count: Int)(text: Int => String): String = {
+    val textVector1 = for i1 <- 1 to count yield text(i1)
+    textVector1.mkString
+  }
+
+  val text1: String = s"""
+package net.scalax.simple.adt
+package impl
+
+trait ADTPassedFunctionImpl extends ADTPassedFunction {
+  ${repeat(22)(i1 => s"""def extraFunctionAdt$i1 = {
+    //
+    //
+    1
+  }
+  """.stripMargin)}
 }
 """.stripMargin
 
---
+++ main/modules/simple-adt/shared/src/codegen/scala-2/net/scalax/simple/adt/impl/ADTPassedFunctionExtra.scala
@@ -0,0 +1,116 @@
+package net.scalax.simple.adt
+package impl
+
+trait ADTPassedFunctionImpl extends ADTPassedFunction {
+  def extraFunctionAdt1 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt2 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt3 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt4 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt5 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt6 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt7 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt8 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt9 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt10 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt11 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt12 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt13 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt14 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt15 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt16 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt17 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt18 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt19 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt20 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt21 = {
+    //
+    //
+    1
+  }
+  def extraFunctionAdt22 = {
+    //
+    //
+    1
+  }
+
+}
