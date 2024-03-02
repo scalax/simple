4899ab1623b7679ec9559444d80d547e552f8899
漓江丽火 <909134790@qq.com>
2024-03-02 10:41:33 +0800

    python pythagoras.

--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number26/RunMain.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number26/RunMain.scala
@@ -1,13 +1,14 @@
-package net.scalax.simple.nat
-package number25_grim_dawn_20240204_01
+package net.scalax.simple
+package nat
+package number26
 
 object RunTest1 {
 
-  def main(arr: Array[String]): Unit = {
+  def main1(arr: Array[String]): Unit = {
     val 人物抗性: 抗性 = 抗性(火焰抗性 = -21, 冰冷抗性 = -46, 闪电抗性 = -47, 毒素抗性 = -11, 穿刺抗性 = -15, 流血抗性 = -48, 活力抗性 = -22, 虚化抗性 = -87, 混乱抗性 = -57)
 
     val (result, list) = utils.count(
-      fumo_sum = utils.fumoSum,
+      fumo_sum = utils.马尔茅斯._2 ++: utils.索拉尔._2 ++: utils.拜斯迈._2 ++: utils.德里格._2,
       base = 人物抗性,
       fumo_list = Vector.fill(7)(("hahjahahah", 抗性.zero)),
       index = 2342,
