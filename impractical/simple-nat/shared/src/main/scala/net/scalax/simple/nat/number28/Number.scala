5ff77fe7f331173def0f3fc2efc00c6fa2b4f807
漓江丽火 <909134790@qq.com>
2024-03-25 04:19:19 +0800

    pythagoras.

--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number28/Number.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number28/Number.scala
@@ -4,7 +4,7 @@ package number28
 
 import ghdmzsk._
 
-object 乘除 {
+object 乘除1 {
 
   trait Num1 {
     def pre1: ghdmzsk
@@ -15,13 +15,13 @@ object 乘除 {
   }
 
   val 被乘数分子: ghdmzsk = new ghdmzsk {
-    override def inputGHDMZSK(tailInput: () => ghdmzsk): ghdmzsk = new ghdmzsk with Num1 {
-      lazy val pre1: ghdmzsk = tailInput()
+    override def inputGHDMZSK(numTail1: () => ghdmzsk): ghdmzsk = new ghdmzsk with Num1 {
+      lazy val pre1: ghdmzsk = numTail1()
 
-      override def inputGHDMZSK(otherInput: () => ghdmzsk): ghdmzsk = new ghdmzsk {
+      override def inputGHDMZSK(numTail2: () => ghdmzsk): ghdmzsk = new ghdmzsk {
         override def inputGHDMZSK(counterTail1: () => ghdmzsk): ghdmzsk = new ghdmzsk {
           override def inputGHDMZSK(counterTail2: () => ghdmzsk): ghdmzsk = {
-            counterTail1().inputGHDMZSK(() => tailInput().inputGHDMZSK(otherInput).inputGHDMZSK(counterTail1).inputGHDMZSK(counterTail2))
+            counterTail1().inputGHDMZSK(() => numTail1().inputGHDMZSK(numTail2).inputGHDMZSK(counterTail1).inputGHDMZSK(counterTail2))
           }
         }
       }
@@ -29,13 +29,13 @@ object 乘除 {
   }
 
   val `被乘数分母/乘数分母`: ghdmzsk = new ghdmzsk {
-    override def inputGHDMZSK(tailInput: () => ghdmzsk): ghdmzsk = new ghdmzsk with Num2 {
-      override lazy val pre2: ghdmzsk = tailInput()
+    override def inputGHDMZSK(numTail1: () => ghdmzsk): ghdmzsk = new ghdmzsk with Num2 {
+      override lazy val pre2: ghdmzsk = numTail1()
 
-      override def inputGHDMZSK(otherInput: () => ghdmzsk): ghdmzsk = new ghdmzsk {
+      override def inputGHDMZSK(numTail2: () => ghdmzsk): ghdmzsk = new ghdmzsk {
         override def inputGHDMZSK(counterTail1: () => ghdmzsk): ghdmzsk = new ghdmzsk {
           override def inputGHDMZSK(counterTail2: () => ghdmzsk): ghdmzsk = {
-            otherInput().inputGHDMZSK(tailInput).inputGHDMZSK(counterTail2).inputGHDMZSK(counterTail1)
+            numTail2().inputGHDMZSK(numTail1).inputGHDMZSK(counterTail2).inputGHDMZSK(counterTail1)
           }
         }
       }
