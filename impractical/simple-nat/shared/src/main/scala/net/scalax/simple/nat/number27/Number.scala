e40af6db78665e306e0a2f94c99b525605bbe7db
漓江丽火 <909134790@qq.com>
2024-03-25 06:24:22 +0800

    pythagoras.

--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number27/Number.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number27/Number.scala
@@ -19,10 +19,7 @@ object 乘除 {
       lazy val pre1: ghdmzsk = tailInput()
 
       override def inputGHDMZSK(otherInput: () => ghdmzsk): ghdmzsk = new ghdmzsk {
-        lazy val compatTail: ghdmzsk = tailInput().inputGHDMZSK(otherInput)
-
         override def inputGHDMZSK(counterTail: () => ghdmzsk): ghdmzsk = {
-          counterTail().inputGHDMZSK(() => compatTail.inputGHDMZSK(counterTail))
           counterTail().inputGHDMZSK(() => tailInput().inputGHDMZSK(otherInput).inputGHDMZSK(counterTail))
         }
       }
@@ -34,15 +31,9 @@ object 乘除 {
       override lazy val pre2: ghdmzsk = taiInput()
 
       override def inputGHDMZSK(otherInput: () => ghdmzsk): ghdmzsk = new ghdmzsk {
-        lazy val compatTail: ghdmzsk = otherInput().inputGHDMZSK(taiInput)
-
         override def inputGHDMZSK(counterTail: () => ghdmzsk): ghdmzsk = {
           val c = if (counterTail() eq 被乘数分子) `被乘数分母/乘数分母` else 被乘数分子
-          // compatTail.inputGHDMZSK(() => c)
-          // otherInput().inputGHDMZSK(counterTail).inputGHDMZSK(taiInput)
-          // counterTail().inputGHDMZSK(() => compatTail.inputGHDMZSK(counterTail))
-          // counterTail().inputGHDMZSK(otherInput).inputGHDMZSK(counterTail).inputGHDMZSK(taiInput)
-          compatTail.inputGHDMZSK(() => c)
+          otherInput().inputGHDMZSK(taiInput).inputGHDMZSK(() => c)
         }
       }
     }
