77c158afe4d59f3ebc39ab5b6228336692e7b367
漓江丽火 <909134790@qq.com>
2024-03-25 20:26:45 +0800

    pythagoras.

--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number29/RunMain.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number29/RunMain.scala
@@ -1,11 +1,10 @@
 package net.scalax.simple
 package nat
-package number27
+package number29
 
 import scala.annotation.tailrec
 
 import ghdmzsk._
-import 乘除._
 
 object RunTest1 {
 
@@ -13,9 +12,9 @@ object RunTest1 {
     def buildImpl(isFenmu: Boolean, numLong: Long, zero: () => ghdmzsk): ghdmzsk = {
       if (numLong > 0) {
         if (isFenmu) {
-          被乘数分子.inputGHDMZSK(() => buildImpl(isFenmu = isFenmu, numLong = numLong - 1, zero))
+          乘除1.被乘数分子.inputGHDMZSK(() => buildImpl(isFenmu = isFenmu, numLong = numLong - 1, zero))
         } else {
-          `被乘数分母/乘数分母`.inputGHDMZSK(() => buildImpl(isFenmu = isFenmu, numLong = numLong - 1, zero))
+          乘除1.被乘数分母.inputGHDMZSK(() => buildImpl(isFenmu = isFenmu, numLong = numLong - 1, zero))
         }
       } else {
         zero()
@@ -49,7 +48,7 @@ object RunTest1 {
 
     if (printlnSum > 0) {
       currentNum match {
-        case num1: Num1 =>
+        case num1: 乘除1.Num1 =>
           countImpl(
             () => num1.pre1,
             current分子 = current分子,
@@ -58,7 +57,7 @@ object RunTest1 {
             printlnSum = if (needPrintln) printlnSum - 1 else printlnSum,
             speed = speed
           )
-        case num2: Num2 =>
+        case num2: 乘除1.Num2 =>
           countImpl(
             () => num2.pre2,
             current分子 = current分子 + 1,
@@ -76,7 +75,9 @@ object RunTest1 {
   def count(num: () => ghdmzsk, except1: BigDecimal, except2: BigDecimal, printlnSum: Int, speed: Long = 8000000): Unit =
     countImpl(num = num, current分子 = 1, current分母 = 1, exceptResult = except1 / except2, printlnSum = printlnSum, speed = speed)
 
-  def main1(arr: Array[String]): Unit = {
+  def main(arr: Array[String]): Unit = {
+    println('3'.toString * 1000)
+
     val 分子1: Long = 123
     val 分母1: Long = 4342
     val 分子2: Long = 328
@@ -88,7 +89,7 @@ object RunTest1 {
     val num1: ghdmzsk = build(分子 = 分子1, 分母 = 分母1)
     val num2: ghdmzsk = build(分子 = 分子2, 分母 = 分母2)
 
-    val result1: () => ghdmzsk = () => num1.inputGHDMZSK(() => num2).inputGHDMZSK(() => 乘除.被乘数分子)
+    val result1: () => ghdmzsk = () => num1.inputGHDMZSK(() => num2).inputGHDMZSK(() => 乘除1.被乘数分母).inputGHDMZSK(() => 乘除1.被乘数分子)
 
     count(result1, except1 = except1, except2 = except2, printlnSum = 18)
 
@@ -101,19 +102,19 @@ object RunTest1 {
 
     val num3: ghdmzsk = build(分子 = 分子3, 分母 = 分母3)
 
-    val result2: () => ghdmzsk = () => result1().inputGHDMZSK(() => num3).inputGHDMZSK(() => 乘除.被乘数分子)
+    val result2: () => ghdmzsk = () => result1().inputGHDMZSK(() => num3).inputGHDMZSK(() => 乘除1.被乘数分母).inputGHDMZSK(() => 乘除1.被乘数分子)
 
-    count(result2, except1 = except3, except2 = except4, printlnSum = 18)
+    count(result2, except1 = except3, except2 = except4, printlnSum = 18, speed = 16000000)
 
     // ===
     val except5: BigDecimal = except3 / except4
     val except6: BigDecimal = except3
 
-    val result3: () => ghdmzsk = () => result2().inputGHDMZSK(result1).inputGHDMZSK(() => 乘除.被乘数分子)
-    count(result3, except1 = except5, except2 = except6, printlnSum = 18)
+    val result3: () => ghdmzsk = () => result2().inputGHDMZSK(result1).inputGHDMZSK(() => 乘除1.被乘数分母).inputGHDMZSK(() => 乘除1.被乘数分子)
+    count(result3, except1 = except5, except2 = except6, printlnSum = 18, speed = 30000000)
 
-    val result4: () => ghdmzsk = () => result2().inputGHDMZSK(result1).inputGHDMZSK(() => 乘除.`被乘数分母/乘数分母`)
-    count(result4, except1 = except6, except2 = except5, printlnSum = 18)
+    val result4: () => ghdmzsk = () => result2().inputGHDMZSK(result1).inputGHDMZSK(() => 乘除1.被乘数分子).inputGHDMZSK(() => 乘除1.被乘数分母)
+    count(result4, except1 = except6, except2 = except5, printlnSum = 18, speed = 30000000)
   }
 
 }
