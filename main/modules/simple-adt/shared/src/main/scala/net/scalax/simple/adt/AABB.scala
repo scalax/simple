46dba1313fbb8f83a7e90b8a073441db1e0e4d5c
djx314 <djx314@sina.cn>
2024-08-03 18:17:21 +0800

    pythagoras.

--- main/modules/simple-adt/shared/src/main/scala/net/scalax/simple/adt/AABB.scala
+++ main/modules/simple-adt/shared/src/main/scala/net/scalax/simple/adt/AABB.scala
@@ -7,7 +7,7 @@ object CCDD {
 
   trait P1
 
-  trait Positive2[+T, Tail[_] <: P1, Data] extends P1 {
+  trait Positive2[T, Tail[_] <: P1, Data] extends P1 {
     def folddd[TD >: Data](f: T => TD): Tail[TD]
     def apply[TD >: Data](f: T => TD): Tail[TD]
   }
@@ -16,18 +16,17 @@ object CCDD {
     def value: Data
   }
 
-  trait CoCoProduct[+PP[_] <: P1] {
-    def P: PP[Nothing]
+  trait Func[T[_] <: P1] {
+    type U[A] = T[A]
+    def instance: T[Nothing]
   }
-  trait CoCoPositive[+Data, PP[_] <: P1] extends CoCoProduct[({ type H2[DABB] = Positive2[Data, PP, DABB] })#H2] {
-    override def P: Positive2[Data, PP, Nothing]
-  }
+  type Append[Data, PP[_] <: P1] = Func[({ type F1[DAB] = Positive2[Data, PP, DAB] })#F1]
+  type Zero                      = Func[({ type F1[DAB] = PZero3[DAB] })#F1]
 
-  def cc: Positive2[
-    Int,
-    ({ type F1[U1] = Positive2[String, ({ type F2[U2] = Positive2[Double, ({ type F3[U3] = PZero3[U3] })#F3, U2] })#F2, U1] })#F1,
-    Nothing
-  ] = ???
+  type AB = Append[Int, Append[String, Append[Double, Zero#U]#U]#U]
+
+  def ab: AB            = ???
+  def cc: AB#U[Nothing] = ab.instance
 
   {
     def dd =
