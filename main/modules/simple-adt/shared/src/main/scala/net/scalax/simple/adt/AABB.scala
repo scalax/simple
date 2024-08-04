70b42eb0e4d7eacbb787d61206b13444b4f91757
djx314 <djx314@sina.cn>
2024-08-04 19:38:52 +0800

    pythagoras.

--- main/modules/simple-adt/shared/src/main/scala/net/scalax/simple/adt/AABB.scala
+++ main/modules/simple-adt/shared/src/main/scala/net/scalax/simple/adt/AABB.scala
@@ -47,4 +47,32 @@ object CCDD {
     def ff: Option[BigDecimal] = ee
   }
 
+  trait PiUU[T <: RuntimeNat] {
+    type U[_] <: P1
+  }
+
+  object PiUU extends PolyA {
+    type Aux[T <: RuntimeNat, U1[_] <: P1] = PiUU[T] {
+      type U[X] = U1[X]
+    }
+
+    implicit def bb: PiUU.Aux[RuntimeZero, ({ type F1[DAB] = PZero3[DAB] })#F1] = ???
+  }
+
+  trait PolyA {
+    implicit def aa[T1 <: RuntimeNat, U1[_] <: P1, Data](implicit
+      cv: PiUU.Aux[T1, U1]
+    ): PiUU.Aux[RuntimeData[Data, T1], ({ type F1[DAB] = Positive2[Data, U1, DAB] })#F1] = ???
+  }
+
+  trait CVAB[Ty <: RuntimeNat] {
+    def fold[U1[_] <: P1](implicit u: PiUU.Aux[Ty, U1]): U1[Nothing] = ???
+  }
+
+  def cvab[Ty <: RuntimeNat]: CVAB[Ty] = new CVAB[Ty] {
+    //
+  }
+
+  val bb = cvab[RuntimeData[BigDecimal, RuntimeData[Int, RuntimeData[String, RuntimeData[Option[BigDecimal], RuntimeZero]]]]].fold
+
 }
