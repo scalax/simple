45c3eeadc0c471b453960c22930d694f7645622e
djx314 <djx314@sina.cn>
2024-08-06 21:53:39 +0800

    pythagoras.

--- wip/simple-codec/shared/src/main/scala/net/scalax/simple/codec/utils/HListUtils2.scala
+++ wip/simple-codec/shared/src/main/scala/net/scalax/simple/codec/utils/HListUtils2.scala
@@ -2,22 +2,57 @@ package net.scalax.simple.codec.utils
 
 import net.scalax.simple.codec.to_list_generic.SimpleProduct
 
+object CCDD {
+
+  trait ABDECD[P1, P1Append[_, _ <: P1] <: P1, P1Zero <: P1, X1, X1Append[_, _ <: X1] <: X1, X1Zero <: X1, Q1, Q1Append[
+    _,
+    _ <: Q1
+  ] <: Q1, Q1Zero <: Q1, M[_, _, _]] {
+    def append[A, P <: P1, X <: X1, Q <: Q1](m: M[P, X, Q]): M[P1Append[A, P], X1Append[A, X], Q1Append[A, Q]]
+    def zero: M[P1Zero, X1Zero, Q1Zero]
+  }
+
+}
+
 trait HListUtils2[Appendable, AppendablePositive[_, _ <: Appendable] <: Appendable, AppendZero <: Appendable] {
   def appendData[Head, Tail <: Appendable](h: Head, t: Tail): AppendablePositive[Head, Tail]
   def takeHead[Head, Tail <: Appendable](dataList: AppendablePositive[Head, Tail]): Head
   def takeTail[Head, Tail <: Appendable](dataList: AppendablePositive[Head, Tail]): Tail
   def takeZero: AppendZero
 
-  def append[T1, HL1[_[_]] <: Appendable](
-    tail: SimpleProduct.Appender[HL1]
-  ): SimpleProduct.Appender[({ type HA[UA[_]] = AppendablePositive[UA[T1], HL1[UA]] })#HA] =
-    new SimpleProduct.Appender[({ type FU[UA[_]] = AppendablePositive[UA[T1], HL1[UA]] })#FU] {
-      override def toHList[M1[_, _, _], M2[_], M3[_], M4[_]](o: SimpleProduct.AppendMonad[M1])(
-        func: SimpleProduct.TypeGen[M1, M2, M3, M4]
-      ): M1[AppendablePositive[M2[T1], HL1[M2]], AppendablePositive[M3[T1], HL1[M3]], AppendablePositive[M4[T1], HL1[M4]]] = {
-        val tailM1: M1[HL1[M2], HL1[M3], HL1[M4]]                             = tail.toHList(o)(func)
-        val headM1: M1[M2[T1], M3[T1], M4[T1]]                                = func[T1]
-        val zipM: M1[(HL1[M2], M2[T1]), (HL1[M3], M3[T1]), (HL1[M4], M4[T1])] = o.zip(tailM1, headM1)
+  def abab[M1[_, _, _], M2[_], M3[_], M4[_]]: CCDD.ABDECD[
+    Appendable,
+    ({ type H1[Head, Tail <: Appendable] = AppendablePositive[M2[Head], Tail] })#H1,
+    AppendZero,
+    Appendable,
+    ({ type H1[Head, Tail <: Appendable] = AppendablePositive[M3[Head], Tail] })#H1,
+    AppendZero,
+    Appendable,
+    ({ type H1[Head, Tail <: Appendable] = AppendablePositive[M4[Head], Tail] })#H1,
+    AppendZero,
+    ({ type MX[A, B, C] = (SimpleProduct.AppendMonad[M1], SimpleProduct.TypeGen[M1, M2, M3, M4]) => M1[A, B, C] })#MX
+  ] =
+    new CCDD.ABDECD[
+      Appendable,
+      ({ type H1[Head, Tail <: Appendable] = AppendablePositive[M2[Head], Tail] })#H1,
+      AppendZero,
+      Appendable,
+      ({ type H1[Head, Tail <: Appendable] = AppendablePositive[M3[Head], Tail] })#H1,
+      AppendZero,
+      Appendable,
+      ({ type H1[Head, Tail <: Appendable] = AppendablePositive[M4[Head], Tail] })#H1,
+      AppendZero,
+      ({ type MX[A, B, C] = (SimpleProduct.AppendMonad[M1], SimpleProduct.TypeGen[M1, M2, M3, M4]) => M1[A, B, C] })#MX
+    ] {
+      override def append[A, P <: Appendable, X <: Appendable, Q <: Appendable](
+        m: (SimpleProduct.AppendMonad[M1], SimpleProduct.TypeGen[M1, M2, M3, M4]) => M1[P, X, Q]
+      ): (
+        SimpleProduct.AppendMonad[M1],
+        SimpleProduct.TypeGen[M1, M2, M3, M4]
+      ) => M1[AppendablePositive[M2[A], P], AppendablePositive[M3[A], X], AppendablePositive[M4[A], Q]] = { (o, func) =>
+        val tailM1: M1[P, X, Q]                          = m(o, func)
+        val headM1: M1[M2[A], M3[A], M4[A]]              = func[A]
+        val zipM: M1[(P, M2[A]), (X, M3[A]), (Q, M4[A])] = o.zip(tailM1, headM1)
 
         o.to(zipM)(
           in1 = t => appendData(t._2, t._1),
@@ -29,21 +64,40 @@ trait HListUtils2[Appendable, AppendablePositive[_, _ <: Appendable] <: Appendab
           in6 = t => (takeTail(t), takeHead(t))
         )
       }
+
+      override def zero
+        : (SimpleProduct.AppendMonad[M1], SimpleProduct.TypeGen[M1, M2, M3, M4]) => M1[AppendZero, AppendZero, AppendZero] = { (o, func) =>
+        o.to(o.zero)(
+          _ => takeZero: AppendZero,
+          _ => takeZero: AppendZero,
+          _ => takeZero: AppendZero
+        )(
+          (_: AppendZero) => (),
+          (_: AppendZero) => (),
+          (_: AppendZero) => ()
+        )
+      }
+    }
+
+  def append[T1, HL1[_[_]] <: Appendable](
+    tail: SimpleProduct.Appender[HL1]
+  ): SimpleProduct.Appender[({ type HA[UA[_]] = AppendablePositive[UA[T1], HL1[UA]] })#HA] =
+    new SimpleProduct.Appender[({ type FU[UA[_]] = AppendablePositive[UA[T1], HL1[UA]] })#FU] {
+      override def toHList[M1[_, _, _], M2[_], M3[_], M4[_]](o: SimpleProduct.AppendMonad[M1])(
+        func: SimpleProduct.TypeGen[M1, M2, M3, M4]
+      ): M1[AppendablePositive[M2[T1], HL1[M2]], AppendablePositive[M3[T1], HL1[M3]], AppendablePositive[M4[T1], HL1[M4]]] = {
+        val c = abab[M1, M2, M3, M4]
+        c.append((x, a) => tail.toHList(x)(a))(o, func)
+      }
     }
 
   val zero: SimpleProduct.Appender[({ type HA[_[_]] = AppendZero })#HA] = new SimpleProduct.Appender[({ type HA[_[_]] = AppendZero })#HA] {
     override def toHList[M1[_, _, _], M2[_], M3[_], M4[_]](
       o: SimpleProduct.AppendMonad[M1]
-    )(func: SimpleProduct.TypeGen[M1, M2, M3, M4]): M1[AppendZero, AppendZero, AppendZero] =
-      o.to(o.zero)(
-        _ => takeZero: AppendZero,
-        _ => takeZero: AppendZero,
-        _ => takeZero: AppendZero
-      )(
-        (_: AppendZero) => (),
-        (_: AppendZero) => (),
-        (_: AppendZero) => ()
-      )
+    )(func: SimpleProduct.TypeGen[M1, M2, M3, M4]): M1[AppendZero, AppendZero, AppendZero] = {
+      val c = abab[M1, M2, M3, M4]
+      c.zero(o, func)
+    }
   }
 
 }
