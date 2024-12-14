682409d4535f4ad0cafd87b7105eae9fa51d5a71
djx314 <djx314@sina.cn>
2024-12-14 08:51:57 +0800

    pythagoras.

--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number33/RunMain.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number33/RunMain.scala
@@ -76,14 +76,15 @@ object RunTest1 {
 
   def main(arr: Array[String]): Unit = {
 
-    val 分子1: Long = 1385
-    val 分母1: Long = 4342
+    val 分子1: Long = 4
+    val 分母1: Long = 1
+
+    val 分子2: Long = 2
+    val 分母2: Long = 1
 
     val except1: BigDecimal = BigDecimal(分子1) / BigDecimal(分母1)
 
-    val num1: ghdmzsk = build(分子 = 分子1, 分母 = 分母1)
-
-    val result1: ghdmzsk = num1.inputGHDMZSK(() => 乘除1.n2).inputGHDMZSK(() => num1.inputGHDMZSK(() => 乘除1.n1))
+    // val result1: ghdmzsk = num1.inputGHDMZSK(() => 乘除1.n2).inputGHDMZSK(() => num1.inputGHDMZSK(() => 乘除1.n1))
     /*.inputGHDMZSK(() => 乘除1.n1)
       .inputGHDMZSK(() => num1.inputGHDMZSK(() => 乘除1.n2))
       .inputGHDMZSK(() => 乘除1.n1)
@@ -91,7 +92,21 @@ object RunTest1 {
       .inputGHDMZSK(() => 乘除1.n1)
       .inputGHDMZSK(() => num1.inputGHDMZSK(() => 乘除1.n2))*/
 
-    count(() => num1, except = except1, printlnSum = 10)
+    val num1: ghdmzsk     = build(分子 = 分子1, 分母 = 分母1)
+    val num1Impl: ghdmzsk = num1.inputGHDMZSK(() => 乘除1.n2)
+    lazy val num1Count: ghdmzsk = new ghdmzsk {
+      override def inputGHDMZSK(t: () => ghdmzsk): ghdmzsk = num1Impl.inputGHDMZSK(() => num1Count.inputGHDMZSK(t))
+    }
+
+    val num2: ghdmzsk           = build(分子 = 分子2, 分母 = 分母2)
+    val num2Impl: ghdmzsk       = num2.inputGHDMZSK(() => 乘除1.n1)
+    lazy val num2Count: ghdmzsk = num2Impl.inputGHDMZSK(() => num2Count)
+
+    val result1: ghdmzsk = num1Impl.inputGHDMZSK(() => num2Impl)
+
+    // val result1: ghdmzsk = num1Count.inputGHDMZSK(() => num2Count)
+
+    // count(() => num1, except = except1, printlnSum = 10)
     count(() => result1, except = except1, printlnSum = 5)
     println("== finished 1 ==")
 
