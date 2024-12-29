1a546c3e551648bfeab437f4bb40f1ea6aee4692
漓江丽火 <djx314@sina.cn>
2024-12-30 05:56:26 +0800

    pytahgoras

--- wip/simple-codec/shared/src/main/scala/net/scalax/simple/codec/SimpleProduct3.scala
+++ wip/simple-codec/shared/src/main/scala/net/scalax/simple/codec/SimpleProduct3.scala
@@ -8,6 +8,8 @@ object SimpleProduct3 {
   trait AppendContext[HListLike, ZeroType <: HListLike, AppendType[_, _ <: HListLike] <: HListLike] {
     def zero: ZeroType
     def append[H, T <: HListLike](h: H, t: T): AppendType[H, T]
+    def takeHead[H, T <: HListLike](a: AppendType[H, T]): H
+    def takeTail[H, T <: HListLike](a: AppendType[H, T]): T
 
     trait ColType {
       type toM[M[_]] <: HListLike
@@ -54,6 +56,29 @@ object SimpleProduct3 {
     trait ColInputType[HL <: ColType, FT <: FType] extends InputType {
       override type toItem  = HL#toM[FT#toF]
       override type AndThen = ColInputType[HL, FT#Next]
+    }
+
+    trait ZipInputType[In1 <: InputType, In2 <: InputType] extends InputType {
+      override type toItem  = (In1#toItem, In2#toItem)
+      override type AndThen = ZipInputType[In1#AndThen, In2#AndThen]
+    }
+
+    // ===
+    trait Mapper[InputA <: InputType, InputB <: InputType] {
+      def map(ia: InputA#toItem): InputB#toItem
+      def reverseMap(ib: InputB#toItem): InputA#toItem
+
+      def nextMapper: Mapper[InputA#AndThen, InputB#AndThen]
+    }
+
+    trait GetSet[Item, HL <: ColType, FT <: FType]
+        extends Mapper[ZipInputType[ItemInputType[Item, FT], ColInputType[HL, FT]], ColInputType[AppendColType[Item, HL], FT]] {
+      override def map(n: (FT#toF[Item], HL#toM[FT#toF])): AppendType[FT#toF[Item], HL#toM[FT#toF]]        = append(n._1, n._2)
+      override def reverseMap(n: AppendType[FT#toF[Item], HL#toM[FT#toF]]): (FT#toF[Item], HL#toM[FT#toF]) = (takeHead(n), takeTail(n))
+
+      override def nextMapper: GetSet[Item, HL, FT#Next] = new GetSet[Item, HL, FT#Next] {
+        //
+      }
     }
 
   }
